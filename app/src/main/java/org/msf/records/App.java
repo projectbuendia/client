7037207cc416435352a58ca2719807164bcc8eb0
DavidChenGoogle <dxchen@google.com>
2014-11-13 10:10:08 -0800

    Implemented skeleton of the auto-update mechanism (without server calls).

--- app/src/main/java/org/msf/records/App.java
+++ app/src/main/java/org/msf/records/App.java
@@ -4,15 +4,36 @@ import android.app.Application;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.squareup.otto.Bus;
+
+import org.msf.records.events.MainThreadBus;
 import org.msf.records.net.BuendiaServer;
 import org.msf.records.net.OpenMrsServer;
 import org.msf.records.net.Server;
+import org.msf.records.updater.UpdateManager;
 import org.odk.collect.android.application.Collect;
 
 /**
  * Created by Gil on 08/10/2014.
  */
 public class App extends Application {
+
+    /**
+     * The current instance of the application.
+     */
+    private static App sInstance;
+
+    /**
+     * An event bus that posts events to any available thread.
+     */
+    private static Bus sBus;
+
+    /**
+     * An event bus that posts events specifically to the main thread.
+     */
+    private static MainThreadBus sMainThreadBus;
+
+    private static UpdateManager mUpdateManager;
 
     private static Server mServer;
 
@@ -25,6 +46,10 @@ public class App extends Application {
                 PreferenceManager.getDefaultSharedPreferences(this);
 
         synchronized (App.class) {
+            sInstance = this;
+            sBus = new Bus();
+            sMainThreadBus = new MainThreadBus(sBus);
+
             if (preferences.getBoolean("use_openmrs", false)) {
                 String openmrsRootUrl = preferences.getString("openmrs_root_url", null);
                 mServer = new OpenMrsServer(getApplicationContext(), openmrsRootUrl);
@@ -33,6 +58,18 @@ public class App extends Application {
                 mServer = new BuendiaServer(getApplicationContext(), apiRootUrl);
             }
         }
+    }
+
+    public static synchronized App getInstance() {
+        return sInstance;
+    }
+
+    public static synchronized Bus getBus() {
+        return sBus;
+    }
+
+    public static synchronized MainThreadBus getMainThreadBus() {
+        return sMainThreadBus;
     }
 
     public static synchronized Server getServer() {
