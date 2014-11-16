e191121dce6c5a64bc7e3b2f27cd8845aab00183
DavidChenGoogle <dxchen@google.com>
2014-11-16 12:55:18 -0800

    UpdateManager now issues HTTP requests.

--- app/src/main/java/org/msf/records/updater/AvailableUpdateInfo.java
+++ app/src/main/java/org/msf/records/updater/AvailableUpdateInfo.java
@@ -1,19 +1,95 @@
 package org.msf.records.updater;
 
 import android.net.Uri;
+import android.util.Log;
+
+import com.github.zafarkhaja.semver.Version;
+
+import org.msf.records.model.UpdateInfo;
 
 /**
  * An object containing information about an available application update.
  */
 public class AvailableUpdateInfo {
 
-    public final int mCurrentVersionCode;
-    public final int mAvailableVersionCode;
+    private static final String TAG = AvailableUpdateInfo.class.getName();
+
+    public final boolean mIsValid;
+    public final Version mCurrentVersion;
+    public final Version mAvailableVersion;
     public final Uri mUpdateUri;
 
-    public AvailableUpdateInfo(int currentVersionCode, int availableVersionCode, Uri updateUri) {
-        mCurrentVersionCode = currentVersionCode;
-        mAvailableVersionCode = availableVersionCode;
+    public static AvailableUpdateInfo getInvalid(Version currentVersion) {
+        return new AvailableUpdateInfo(
+                false /*isValid*/,
+                currentVersion,
+                UpdateManager.INVALID_VERSION,
+                null /*updateUri*/);
+    }
+
+    public static AvailableUpdateInfo fromResponse(Version currentVersion, UpdateInfo response) {
+        if (response == null) {
+            Log.w(TAG, "The update info is null.");
+            return getInvalid(currentVersion);
+        }
+
+        if (response.androidClient == null) {
+            Log.w(TAG, "The update info response is missing the androidclient field.");
+            return getInvalid(currentVersion);
+        }
+
+        Version version = response.androidClient.getLatestVersion();
+        if (version == null) {
+            Log.w(
+                    TAG,
+                    "The androidclient update info response is missing the version field or its "
+                            + "version field is not a valid semantic version."
+            );
+            return getInvalid(currentVersion);
+        }
+
+        if (response.androidClient.run == null || response.androidClient.run.size() < 1) {
+            Log.w(TAG, "The androidclient update info response does not have a run entry.");
+            return getInvalid(currentVersion);
+        }
+
+        String runEntry = response.androidClient.run.get(1);
+        if (runEntry == null) {
+            Log.w(TAG, "The androidclient update info response's run entry is null.");
+            return getInvalid(currentVersion);
+        }
+
+        Uri updateUri;
+        try {
+            updateUri = Uri.parse(UpdateServer.ROOT_URL + runEntry);
+        } catch (IllegalArgumentException e) {
+            Log.w(
+                    TAG,
+                    "The androidclient update info response's run entry is not a valid URI path "
+                            + "segment.",
+                    e
+            );
+            return getInvalid(currentVersion);
+        }
+
+        return new AvailableUpdateInfo(true /*isValid*/, currentVersion, version, updateUri);
+    }
+
+    private AvailableUpdateInfo(
+            boolean isValid,
+            Version currentVersion,
+            Version availableVersion,
+            Uri updateUri) {
+        mIsValid = isValid;
+        mCurrentVersion = currentVersion;
+        mAvailableVersion = availableVersion;
         mUpdateUri = updateUri;
+    }
+
+    /**
+     * Returns whether this update is valid and is newer than current version.
+     */
+    public boolean shouldUpdate() {
+        return mIsValid && mAvailableVersion.greaterThan(mCurrentVersion);
     }
 }
--- app/src/main/java/org/msf/records/updater/UpdateManager.java
+++ app/src/main/java/org/msf/records/updater/UpdateManager.java
@@ -1,12 +1,19 @@
 package org.msf.records.updater;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.android.volley.Response;
+import com.github.zafarkhaja.semver.ParseException;
+import com.github.zafarkhaja.semver.Version;
 import com.squareup.otto.Bus;
 
 import org.joda.time.DateTime;
@@ -14,11 +21,16 @@ import org.msf.records.App;
 import org.msf.records.events.UpdateAvailableEvent;
 import org.msf.records.events.UpdateDownloadedEvent;
 import org.msf.records.events.UpdateNotAvailableEvent;
+import org.msf.records.model.UpdateInfo;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * An object that manages auto-updating of the application from a configurable update server.
+ *
+ * <p>This class requires that all methods be called from the main thread.
  */
 public class UpdateManager {
 
@@ -32,12 +44,20 @@ public class UpdateManager {
     public static final int CHECK_FOR_UPDATE_FREQUENCY_HOURS = 24;
 
     /**
-     * The version code of a downloaded update that is invalid.
+     * An invalid semantic version.
+     *
+     * <p>This value is smaller than any other semantic version. If the current application has this
+     * version, any valid update will be installed over it. If an update has this version, it will
+     * never be installed over any the current application.
      */
-    public static final int INVALID_VERSION_CODE = -1;
+    public static final Version INVALID_VERSION = Version.forIntegers(0);
 
+    private final Object mLock = new Object();
+
+    private final UpdateServer mServer;
     private final PackageManager mPackageManager;
-    private final int mCurrentVersionCode;
+    private final Version mCurrentVersion;
+    private final DownloadManager mDownloadManager;
 
     private DateTime mLastCheckForUpdateTime = new DateTime(0 /*instant*/);
     private AvailableUpdateInfo mLastAvailableUpdateInfo = null;
@@ -46,11 +66,18 @@ public class UpdateManager {
     // so that we can check for it on application launch.
     private DownloadedUpdateInfo mLastDownloadedUpdateInfo = null;
 
+    private final Object mDownloadLock = new Object();
     private boolean mIsDownloadInProgress = false;
+    private long mDownloadId = -1;
 
     public UpdateManager() {
+        mServer = new UpdateServer(null /*rootUrl*/);
         mPackageManager = App.getInstance().getPackageManager();
-        mCurrentVersionCode = getCurrentVersionCode();
+        mDownloadManager =
+                (DownloadManager) App.getInstance().getSystemService(Context.DOWNLOAD_SERVICE);
+        mCurrentVersion = getCurrentVersion();
+        mLastAvailableUpdateInfo = AvailableUpdateInfo.getInvalid(mCurrentVersion);
+        mLastDownloadedUpdateInfo = DownloadedUpdateInfo.getInvalid(mCurrentVersion);
     }
 
     /**
@@ -81,7 +108,12 @@ public class UpdateManager {
      *            {@link org.msf.records.events.MainThreadBus}.
      */
     public void checkForUpdate(Bus bus) {
-        new CheckForUpdateTask(bus, mCurrentVersionCode).execute();
+        DateTime now = DateTime.now();
+        if (now.isAfter(mLastCheckForUpdateTime.plusHours(CHECK_FOR_UPDATE_FREQUENCY_HOURS))) {
+            mLastCheckForUpdateTime = now;
+
+            mServer.getAndroidUpdateInfo(new CheckForUpdateResponseListener(bus), null, null);
+        }
     }
 
     /**
@@ -91,9 +123,28 @@ public class UpdateManager {
      * @param bus the event bus to which to post update downloaded events. If called from an
      *            activity or a service, this should be an instance of
      *            {@link org.msf.records.events.MainThreadBus}.
+     * @return whether a download was started. {@code false} if a download is already in progress.
      */
-    public void downloadUpdate(Bus bus, AvailableUpdateInfo updateInfo) {
-        new DownloadUpdateTask(bus).execute();
+    public boolean downloadUpdate(Bus bus, AvailableUpdateInfo availableUpdateInfo) {
+        synchronized (mDownloadLock) {
+            if (mIsDownloadInProgress) {
+                return false;
+            }
+
+            DownloadManager.Request request =
+                    new DownloadManager.Request(availableUpdateInfo.mUpdateUri)
+                            .setTitle(
+                                    "Downloading update v"
+                                            + availableUpdateInfo.mAvailableVersion.toString())
+                            .setDestinationInExternalFilesDir(
+                                    App.getInstance(),
+                                    null /*dirType*/,
+                                    "androidclient_"
+                                            + availableUpdateInfo.mAvailableVersion.toString());
+            mDownloadId = mDownloadManager.enqueue(request);
+
+            return true;
+        }
     }
 
     /**
@@ -115,99 +166,141 @@ public class UpdateManager {
     }
 
     /**
-     * Returns the version code of the application.
+     * Returns the semantic version of the application.
      */
-    private int getCurrentVersionCode() {
+    private Version getCurrentVersion() {
         PackageInfo packageInfo;
         try {
             packageInfo =
                     mPackageManager.getPackageInfo(App.getInstance().getPackageName(), 0 /*flags*/);
-        } catch (PackageManager.NameNotFoundException ex) {
+        } catch (PackageManager.NameNotFoundException e) {
             Log.wtf(
                     TAG,
                     "No package found with the name " + App.getInstance().getPackageName() + ". "
-                            + "This should never happen.");
-            return INVALID_VERSION_CODE;
+                            + "This should never happen.",
+                    e);
+            return INVALID_VERSION;
         }
 
-        return packageInfo.versionCode;
+        try {
+            return Version.valueOf(packageInfo.versionName);
+        } catch (ParseException e) {
+            Log.e(
+                    TAG,
+                    "Application has an invalid semantic version: " + packageInfo.versionName + ". "
+                            + "Please fix in build.gradle.",
+                    e);
+            return INVALID_VERSION;
+        }
     }
 
     /**
-     * An {@link AsyncTask} that checks for an application update and fires an appropriate event.
+     * A {@link Response.Listener} that handles check-for-update responses.
      */
-    private class CheckForUpdateTask extends AsyncTask<Void, Void, Void> {
+    private class CheckForUpdateResponseListener implements Response.Listener<UpdateInfo> {
 
         private final Bus mBus;
-        private final int mCurrentVersionCode;
 
-        public CheckForUpdateTask(Bus bus, int currentVersionCode) {
+        public CheckForUpdateResponseListener(Bus bus) {
             mBus = bus;
-            mCurrentVersionCode = currentVersionCode;
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
-            DateTime now = DateTime.now();
-            if (now.isAfter(mLastCheckForUpdateTime.plusHours(CHECK_FOR_UPDATE_FREQUENCY_HOURS))) {
-                mLastCheckForUpdateTime = now;
-                // TODO(dxchen): Actually issue an RPC request to check for updates. For now, we
-                // will just fake it.
-                mLastAvailableUpdateInfo = new AvailableUpdateInfo(
-                        mCurrentVersionCode, mCurrentVersionCode, Uri.EMPTY);
-            }
+        public void onResponse(UpdateInfo response) {
+            synchronized (mLock) {
+                mLastAvailableUpdateInfo =
+                        AvailableUpdateInfo.fromResponse(mCurrentVersion, response);
 
-            if (mLastAvailableUpdateInfo != null) {
-                // If there's already a downloaded update that is as recent as the available update,
-                // immediately post an UpdateDownloadedEvent; otherwise, post an
-                // UpdateAvailableEvent.
-                if (mLastDownloadedUpdateInfo != null
-                        && (mLastAvailableUpdateInfo.mAvailableVersionCode
-                                <= mLastDownloadedUpdateInfo.mDownloadedVersionCode)) {
+                if (mLastDownloadedUpdateInfo.shouldInstall()
+                        && mLastDownloadedUpdateInfo.mDownloadedVersion
+                                .greaterThanOrEqualTo(mLastAvailableUpdateInfo.mAvailableVersion)) {
+                    // If there's already a downloaded update that is as recent as the available
+                    // update, post an UpdateDownloadedEvent.
                     mBus.post(new UpdateDownloadedEvent(mLastDownloadedUpdateInfo));
-                } else {
+                } else if (mLastAvailableUpdateInfo.shouldUpdate()) {
+                    // Else, if the latest available update is good, post an UpdateAvailableEvent.
                     mBus.post(new UpdateAvailableEvent(mLastAvailableUpdateInfo));
+                } else {
+                    // Else, post an UpdateNotAvailableEvent.
+                    mBus.post(new UpdateNotAvailableEvent());
                 }
-            } else {
-                mBus.post(new UpdateNotAvailableEvent());
             }
-
-            return null;
         }
     }
 
     /**
-     * An {@link AsyncTask} that downloads an available update.
+     * A {@link BroadcastReceiver} that listens for
+     * {@code DownloadManager.ACTION_DOWNLOAD_COMPLETED} intents.
      */
-    private class DownloadUpdateTask extends AsyncTask<Void, Void, Void> {
+    private class DownloadUpdateReceiver extends BroadcastReceiver {
 
         private final Bus mBus;
 
-        public DownloadUpdateTask(Bus bus) {
+        private DownloadUpdateReceiver(Bus bus) {
             mBus = bus;
         }
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            // If another download is in progress, simply return. Mutates to mIsDownloadInProgress
-            // are not thread-safe but our AsyncTasks are configured to execute sequentially, so
-            // we're okay.
-            if (mIsDownloadInProgress) {
-                return null;
-            }
-            mIsDownloadInProgress = true;
+        public void onReceive(Context context, Intent intent) {
+            synchronized (mDownloadLock) {
+                if (!mIsDownloadInProgress) {
+                    Log.wtf(
+                            TAG,
+                            "Received an ACTION_DOWNLOAD_COMPLETED intent when no download was in "
+                                    + "progress. This indicates that this receiver was registered "
+                                    + "incorrectly. Unregistering receiver.");
+                    App.getInstance().unregisterReceiver(this);
+                    return;
+                }
 
-            try {
-                // TODO(dxchen): Actually issue an HTTP request to download the update. For now, we
-                // will just fake it.
+                long receivedDownloadId =
+                        intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+                if (mDownloadId != receivedDownloadId) {
+                    Log.d(
+                            TAG,
+                            "Received an ACTION_DOWNLOAD_COMPLETED intent with download ID "
+                                    + receivedDownloadId + " when the expected download ID is "
+                                    + mDownloadId + ". Download was probably initiated by another "
+                                    + " application.");
+                    return;
+                }
+
+
+                Cursor cursor = mDownloadManager.query(
+                        new DownloadManager.Query().setFilterById(receivedDownloadId));
+                if (!cursor.moveToFirst()) {
+                    Log.w(TAG, "Received download ID " + receivedDownloadId + " does not exist.");
+                    // TODO(dxchen): Consider firing an event.
+                    return;
+                }
+
+                int status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                if (status != DownloadManager.STATUS_SUCCESSFUL) {
+                    Log.w(TAG, "Update download failed with status " + status + ".");
+                    // TODO(dxchen): Consider firing an event.
+                    return;
+                }
+
+                String uriString =
+                        cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                if (uriString == null) {
+                    Log.w(TAG, "No path for a downloaded file exists.");
+                    // TODO(dxchen): Consider firing an event.
+                    return;
+                }
+
+                Uri uri;
+                try {
+                    Uri.parse(uriString);
+                } catch (IllegalArgumentException e) {
+                    Log.w(TAG, "Path for downloaded file is invalid: " + uriString + ".", e);
+                    // TODO(dxchen): Consider firing an event.
+                    return;
+                }
+
+                // TODO(dxchen): Get the path from the local URI.
                 mLastDownloadedUpdateInfo =
-                        new DownloadedUpdateInfo("/sdcard/org.msf.records/app-debug.apk");
-
-                mBus.post(new UpdateDownloadedEvent(mLastDownloadedUpdateInfo));
-
-                return null;
-            } finally {
-                mIsDownloadInProgress = false;
+                        DownloadedUpdateInfo.fromPath(mCurrentVersion, uriString);
             }
         }
     }
