30cfb5825310de35b11b4612b74be330ab06bf0f
Ka-Ping Yee <ping@zesty.ca>
2014-11-18 04:21:38 +0000

    Use URL encoding for URL query parameters.

--- app/src/main/java/org/msf/records/net/Constants.java
+++ app/src/main/java/org/msf/records/net/Constants.java
@@ -12,17 +12,20 @@ public class Constants {
     public static final String LOCALHOST_EMULATOR = "10.0.2.2";
 
     /**
-     * The base URL for an OpenMRS REST API from the Project Buendia module.
+     * The base path for the OpenMRS REST API from the Project Buendia module.
      */
-    public static final String API_BASE = "/openmrs/ws/rest/v1/projectbuendia";
+    public static final String API_PATH = "/openmrs/ws/rest/v1/projectbuendia";
+
+    /**
+     * The base URL for the OpenMRS REST API from the Project Buendia module.
+     */
+    public static final String API_URL = "http://" + LOCALHOST_EMULATOR + ":8080" + API_PATH;
 
     /**
      * Recommended user for local admin in development.
      */
     public static final String LOCAL_ADMIN_USERNAME = "admin";
 
-    /**
-     * Recommended password for local admin in development.
-     */
+    /** Recommended password for local admin in development. */
     public static final String LOCAL_ADMIN_PASSWORD = "Admin123";
 }
