1bc4d0747542237983e487020c53ec5c3dfbe56f
Nick Fortescue <nfortescue@google.com>
2014-11-19 23:40:02 +0000

    Make status code more robust to failure and log an error.

--- app/src/main/java/org/msf/records/model/Status.java
+++ app/src/main/java/org/msf/records/model/Status.java
@@ -2,6 +2,7 @@ package org.msf.records.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Log;
 
 import org.msf.records.R;
 
@@ -43,7 +44,7 @@ public class Status implements Parcelable, ListItem {
         STATUSES.put("probable", new Status("probable", R.string.status_probable, R.drawable.square_probable, R.drawable.round_probable, R.color.status_probable));
         STATUSES.put("confirmed", new Status("confirmed", R.string.status_confirmed, R.drawable.square_confirmed, R.drawable.round_confirmed, R.color.status_confirmed));
         STATUSES.put("non-case", new Status("non-case", R.string.status_non_case, R.drawable.square_non_case, R.drawable.round_non_case, R.color.status_non_case));
-        STATUSES.put("convalescence", new Status("convalescence", R.string.status_convalescence, R.drawable.square_convalescence, R.drawable.round_convalescent, R.color.status_convalescent));
+        STATUSES.put("convalescent", new Status("convalescent", R.string.status_convalescence, R.drawable.square_convalescence, R.drawable.round_convalescent, R.color.status_convalescent));
         STATUSES.put("can-be-discharged", new Status("can-be-discharged", R.string.status_can_be_discharged, R.drawable.square_can_be_discharged, R.drawable.round_can_be_discharged, R.color.status_can_be_discharged));
         STATUSES.put("discharged", new Status("discharged", R.string.status_discharged, R.drawable.square_discharged, R.drawable.round_discharged, R.color.status_discharged));
         STATUSES.put("unconfirmed-death", new Status("unconfirmed-death", R.string.status_unconfirmed_death, R.drawable.square_unconfirmed_death, R.drawable.round_deceased_unconfirmed, R.color.status_unconfirmed_death));
@@ -57,7 +58,12 @@ public class Status implements Parcelable, ListItem {
     }
 
     public static Status getStatus(String statusKey){
-        return STATUSES.get(statusKey);
+        Status status = STATUSES.get(statusKey);
+        if (status == null) {
+            Log.e("Status", "Tried to get status for unknown: " + statusKey);
+            return STATUSES.get("suspected");
+        }
+        return status;
     }
 
     @Override
