56ed23d4d871c10691363e439fbb3a0d6806e0b0
DavidChenGoogle <dxchen@google.com>
2014-11-19 14:07:38 +0000

    Minor widget tweaks

--- odkcollect/src/main/java/org/odk/collect/android/widgets/DateWidget.java
+++ odkcollect/src/main/java/org/odk/collect/android/widgets/DateWidget.java
@@ -121,19 +121,23 @@ public class DateWidget extends QuestionWidget {
 	private void hideDayFieldIfNotInFormat(FormEntryPrompt prompt) {
         String appearance = prompt.getQuestion().getAppearanceAttr();
         if ( appearance == null ) {
-        	if ( Build.VERSION.SDK_INT >= 11 ) {
-        		showCalendar = true;
-	        	this.mDatePicker.setCalendarViewShown(true);
-	        	if ( Build.VERSION.SDK_INT >= 12 ) {
-	        		CalendarView cv = this.mDatePicker.getCalendarView();
-		        	cv.setShowWeekNumber(false);
-	        	}
-	        	this.mDatePicker.setSpinnersShown(true);
-	        	hideDay = true;
-	        	hideMonth = false;
-        	} else {
-        		return;
-        	}
+//        	if ( Build.VERSION.SDK_INT >= 11 ) {
+//        		showCalendar = true;
+//	        	this.mDatePicker.setCalendarViewShown(true);
+//	        	if ( Build.VERSION.SDK_INT >= 12 ) {
+//	        		CalendarView cv = this.mDatePicker.getCalendarView();
+//		        	cv.setShowWeekNumber(false);
+//	        	}
+//	        	this.mDatePicker.setSpinnersShown(true);
+//	        	hideDay = true;
+//	        	hideMonth = false;
+//        	} else {
+//        		return;
+//        	}
+            if ( Build.VERSION.SDK_INT >= 11 ) {
+                this.mDatePicker.setCalendarViewShown(false);
+                this.mDatePicker.setSpinnersShown(true);
+            }
         } else if ( "month-year".equals(appearance) ) {
         	hideDay = true;
         	if ( Build.VERSION.SDK_INT >= 11 ) {
