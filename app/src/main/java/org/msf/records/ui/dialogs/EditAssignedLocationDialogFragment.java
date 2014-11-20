66b865b5551c9416e7fbd5c1d6e049e3729e4f4b
DavidChenGoogle <dxchen@google.com>
2014-11-20 18:45:08 +0000

    Started work

---
+++ app/src/main/java/org/msf/records/ui/dialogs/EditAssignedLocationDialogFragment.java
@@ -0,0 +1,26 @@
+package org.msf.records.ui.dialogs;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A {@link DialogFragment} for editing a user's assigned location.
+ */
+public class EditAssignedLocationDialogFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //inflater.inflate
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
