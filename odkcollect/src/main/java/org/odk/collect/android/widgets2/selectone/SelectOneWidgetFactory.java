9712afe1ee8a1d508a2e18fbc6d6cb34baccf5d2
DavidChenGoogle <dxchen@google.com>
2014-11-20 03:20:01 +0000

    First pass at better date widget

--- odkcollect/src/main/java/org/odk/collect/android/widgets2/selectone/SelectOneWidgetFactory.java
+++ odkcollect/src/main/java/org/odk/collect/android/widgets2/selectone/SelectOneWidgetFactory.java
@@ -2,33 +2,28 @@ package org.odk.collect.android.widgets2.selectone;
 
 import android.content.Context;
 
-import org.javarosa.core.model.data.IAnswerData;
 import org.javarosa.core.model.data.SelectOneData;
 import org.javarosa.form.api.FormEntryPrompt;
 import org.odk.collect.android.widgets2.Appearance;
 import org.odk.collect.android.widgets2.TypedWidget;
 import org.odk.collect.android.widgets2.TypedWidgetFactory;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
- * A {@link org.odk.collect.android.widgets2.TypedWidgetFactory} that creates {@link SelectOneData}
- * widgets.
+ * A {@link TypedWidgetFactory} that creates {@link SelectOneData} widgets.
  */
 public class SelectOneWidgetFactory extends TypedWidgetFactory<SelectOneData> {
 
-    @Override
     public TypedWidget<SelectOneData> create(
-            Context context, FormEntryPrompt prompt, boolean forceReadOnly) {
-        Appearance appearance = Appearance.fromString(prompt.getAppearanceHint());
+            Context context, FormEntryPrompt prompt, Appearance appearance, boolean forceReadOnly) {
+        // TODO(dxchen):
+
 //        if (appearance == null) {
 //            return null;
 //        }
 
 //        if (appearance.mPrimaryAppearance.equals("minimal")
 //                && appearance.hasQualifier("segmented")) {
-            return new SegmentedRadioWidget(context, prompt, forceReadOnly);
+            return new ButtonsSelectOneWidget(context, prompt, appearance, forceReadOnly);
 //        }
 
 //        return null;
