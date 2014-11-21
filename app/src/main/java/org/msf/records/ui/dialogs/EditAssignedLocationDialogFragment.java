854ab1b2b04392b999c685f0bcd5e83723e3647b
DavidChenGoogle <dxchen@google.com>
2014-11-21 11:48:14 +0000

    First pass

--- app/src/main/java/org/msf/records/ui/dialogs/EditAssignedLocationDialogFragment.java
+++ app/src/main/java/org/msf/records/ui/dialogs/EditAssignedLocationDialogFragment.java
@@ -7,16 +7,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.msf.records.R;
+import org.msf.records.view.InstantAutoCompleteTextView;
+
+import butterknife.InjectView;
+
 /**
  * A {@link DialogFragment} for editing a user's assigned location.
  */
 public class EditAssignedLocationDialogFragment extends DialogFragment {
 
+    @InjectView(R.id.zone)
+    InstantAutoCompleteTextView zone;
+
+    @InjectView(R.id.tent)
+    InstantAutoCompleteTextView tent;
+
+    @InjectView(R.id.bed)
+    InstantAutoCompleteTextView bed;
+
     @Nullable
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //inflater.inflate
+        View fragment = inflater.inflate(R.layout.dialog_fragment_edit_assigned_location, null);
+
+        return fragment;
     }
 
     @Override
