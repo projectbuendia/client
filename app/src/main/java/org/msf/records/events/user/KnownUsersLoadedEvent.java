e1d89f2825be3c79e018e2986f2a29a971f546fe
DavidChenGoogle <dxchen@google.com>
2014-11-22 16:43:32 +0000

    Added UserManager as the interface for managing users

---
+++ app/src/main/java/org/msf/records/events/user/KnownUsersLoadedEvent.java
@@ -0,0 +1,7 @@
+package org.msf.records.events.user;
+
+/**
+ * An event bus event indicating that the set of known users has been loaded from local cache.
+ */
+public class KnownUsersLoadedEvent {
+}
---
+++ app/src/main/java/org/msf/records/events/user/KnownUsersSyncFailedEvent.java
@@ -0,0 +1,9 @@
+package org.msf.records.events.user;
+
+/**
+ * An event bus event indicating that the set of known users failed to be synced from the server.
+ */
+public class KnownUsersSyncFailedEvent {
+
+    public static final int REASON_UNKNOWN = 0;
+}
