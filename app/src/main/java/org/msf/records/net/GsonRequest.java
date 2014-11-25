c0444c4497fd7433f4245e334d8bca4d02fecb46
Nick Fortescue <nfortescue@google.com>
2014-11-25 14:53:17 +0000

    Refactored the network connection code to remove duplication. Added a link to the patient encounters endpoint that doesn't do much.

--- app/src/main/java/org/msf/records/net/GsonRequest.java
+++ app/src/main/java/org/msf/records/net/GsonRequest.java
@@ -7,6 +7,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
@@ -15,11 +16,14 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Map;
 
+import javax.annotation.Nullable;
+
 /**
  * Created by Gil on 08/10/2014.
  */
 public class GsonRequest<T> extends Request<T> {
-    private final Gson gson = new Gson();
+
+    private final GsonBuilder gson = new GsonBuilder();
     private final Class<T> clazz;
     private final Map<String, String> headers;
     private final Response.Listener<T> listener;
@@ -35,13 +39,12 @@ public class GsonRequest<T> extends Request<T> {
      */
     public GsonRequest(String url, Class clazz, boolean array, Map<String, String> headers,
                        Response.Listener<T> listener, Response.ErrorListener errorListener) {
-        super(Method.GET, url, errorListener);
-        this.clazz = clazz;
-        this.headers = headers;
-        this.listener = listener;
-        this.array = array;
+        this(Method.GET, null, url, clazz, array, headers, listener, errorListener);
     }
-    public GsonRequest(int method, Map<String, String> body, String url, Class clazz, boolean array, Map<String, String> headers,
+
+    public GsonRequest(int method,
+                       @Nullable Map<String, String> body,
+                       String url, Class clazz, boolean array, Map<String, String> headers,
                        Response.Listener<T> listener, Response.ErrorListener errorListener) {
         super(method, url, errorListener);
         this.body = body;
@@ -72,17 +75,18 @@ public class GsonRequest<T> extends Request<T> {
             String json = new String(
                     response.data,
                     HttpHeaderParser.parseCharset(response.headers));
+            Gson gsonParser = gson.create();
             if(array){
                 JsonParser parser = new JsonParser();
                 JsonArray array = (JsonArray) parser.parse(json);
                 ArrayList elements = new ArrayList();
                 for (int i = 0; i < array.size(); i++) {
-                    elements.add(gson.fromJson(array.get(i).toString(), clazz));
+                    elements.add(gsonParser.fromJson(array.get(i).toString(), clazz));
                 }
                 return (Response<T>) Response.success(elements, HttpHeaderParser.parseCacheHeaders(response));
             } else {
                 return Response.success(
-                        gson.fromJson(json, clazz),
+                        gsonParser.fromJson(json, clazz),
                         HttpHeaderParser.parseCacheHeaders(response));
             }
         } catch (UnsupportedEncodingException e) {
@@ -92,5 +96,9 @@ public class GsonRequest<T> extends Request<T> {
         } catch (Exception e){
             return Response.error(new ParseError(e));
         }
+    }
+
+    public GsonBuilder getGson() {
+        return gson;
     }
 }
