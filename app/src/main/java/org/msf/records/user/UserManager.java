7ea4aae42ae57523d7e3ba25b8146dd815826f59
akalachman <akalachman@gmail.com>
2014-11-24 16:54:19 -0800

    Commiting real user list change again

--- app/src/main/java/org/msf/records/user/UserManager.java
+++ app/src/main/java/org/msf/records/user/UserManager.java
@@ -73,7 +73,7 @@ public class UserManager {
     private User mActiveUser;
 
     public UserManager() {
-        mUserStore = new FakeUserStore();
+        mUserStore = new UserStore();
     }
 
     /**
--- app/src/main/java/org/msf/records/user/UserStore.java
+++ app/src/main/java/org/msf/records/user/UserStore.java
@@ -1,16 +1,51 @@
 package org.msf.records.user;
 
+import android.util.Log;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.msf.records.App;
 import org.msf.records.model.User;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * A store for users.
  */
 public class UserStore {
+    private final String TAG = "UserStore";
 
     public Set<User> loadKnownUsers() {
-        throw new UnsupportedOperationException();
+        // Make an async call to the server and use a CountDownLatch to block until the result is
+        // returned.
+        final CountDownLatch latch = new CountDownLatch(1);
+        final Set<User> users = new HashSet<User>();
+        App.getServer().listUsers(
+                null,
+                new Response.Listener<List<User>>() {
+                    @Override
+                    public void onResponse(List<User> response) {
+                        users.addAll(response);
+                        latch.countDown();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "Unexpected error loading user list", error);
+                        latch.countDown();
+                    }
+                }, TAG);
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            Log.e(TAG, "Interrupted while loading user list", e);
+        }
+        return users;
     }
 
     public Set<User> syncKnownUsers() {
