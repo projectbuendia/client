6c9ab09fe87e99ddfe8111557f26960f9ccbe3dd
DavidChenGoogle <dxchen@google.com>
2014-11-27 15:27:01 +0000

    Created new PatientChartActivity, which supersedes the PatientDetailActivity, and hooked it up

---
+++ app/src/main/java/org/msf/records/ui/PatientChartFragment.java
@@ -0,0 +1,84 @@
+package org.msf.records.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.msf.records.App;
+import org.msf.records.R;
+import org.msf.records.model.ChartStructure;
+import org.msf.records.model.ConceptList;
+import org.msf.records.model.PatientChart;
+import org.msf.records.net.OpenMrsChartServer;
+
+import java.util.Arrays;
+
+/**
+ * Displays a chart for a given patient. For now, this is actually
+ * just a demo image.
+ */
+public class PatientChartFragment extends Fragment {
+    private static final String TAG = "PatientDetailViewChartFragment";
+
+    public static PatientChartFragment newInstance(String patientUuid) {
+        PatientChartFragment fragment = new PatientChartFragment();
+        OpenMrsChartServer server = new OpenMrsChartServer(App.getConnectionDetails());
+        // TODO(nfortescue): get proper caching, and the dictionary working.
+        server.getChart(patientUuid, new Response.Listener<PatientChart>() {
+            @Override
+            public void onResponse(PatientChart response) {
+                Log.i(TAG, response.uuid + " " + Arrays.asList(response.encounters));
+            }
+        },
+        new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Unexpected error on fetching chart", error);
+            }
+        });
+        server.getConcepts(
+                new Response.Listener<ConceptList>() {
+                    @Override
+                    public void onResponse(ConceptList response) {
+                        Log.i(TAG, Integer.toString(response.results.length));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "Unexpected error fetching concepts", error);
+                    }
+                });
+        server.getChartStructure("ea43f213-66fb-4af6-8a49-70fd6b9ce5d4",
+                new Response.Listener<ChartStructure>() {
+                    @Override
+                    public void onResponse(ChartStructure response) {
+                        Log.i(TAG, Arrays.asList(response.groups).toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "Unexpected error fetching concepts", error);
+                    }
+                });
+        return fragment;
+    }
+
+    public PatientChartFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_patient_chart, container, false);
+    }
+}
