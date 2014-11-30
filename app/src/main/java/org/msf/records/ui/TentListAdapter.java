a8bba39d59034b2407f32088c6b2e9e9b96e679c
akalachman <akalachman@gmail.com>
2014-11-30 17:49:39 +0000

    Fakes the tent list

---
+++ app/src/main/java/org/msf/records/ui/TentListAdapter.java
@@ -0,0 +1,42 @@
+package org.msf.records.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import org.msf.records.R;
+import org.msf.records.view.SubtitledButtonView;
+
+/**
+ * Created by akalachman on 11/30/14.
+ */
+// TODO(akalachman): Replace with retrieval of real data.
+public class TentListAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final String[] values;
+
+    private final String DEFAULT_SUBTITLE = "34 patients";
+
+    public TentListAdapter(Context context, String[] values) {
+        super(context, R.layout.listview_cell_tent_selection, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(
+                R.layout.listview_cell_tent_selection, parent, false);
+
+        SubtitledButtonView button =
+                (SubtitledButtonView)rowView.findViewById(R.id.tent_selection_tent);
+        button.setTitle(values[position]);
+        button.setSubtitle(DEFAULT_SUBTITLE);
+
+        return rowView;
+    }
+}
--- app/src/main/java/org/msf/records/ui/TentSelectionFragment.java
+++ app/src/main/java/org/msf/records/ui/TentSelectionFragment.java
@@ -5,10 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import org.msf.records.R;
 
-public class TentSelectionFragment extends Fragment {
+public class TentSelectionFragment extends Fragment implements
+        AdapterView.OnItemClickListener{
+    private GridView mTentGrid;
+
     public static TentSelectionFragment newInstance() {
         TentSelectionFragment fragment = new TentSelectionFragment();
         return fragment;
@@ -28,5 +33,25 @@ public class TentSelectionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tent_selection, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mTentGrid = (GridView) view.findViewById(R.id.tent_selection_tents);
+        mTentGrid.setOnItemClickListener(this);
+
+        // TODO(akalachman): Remove and allow adapter to retrieve its own list.
+        String[] values = new String[] {
+                "S1", "S2", "P1", "P2", "C1", "C2", "C3", "C4", "C5", "C6", "Morgue"
+        };
+        TentListAdapter adapter = new TentListAdapter(getActivity(), values);
+        mTentGrid.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // TODO(akalachman): Implement.
     }
 }
