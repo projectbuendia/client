17d6c33d56bdafc216c92867a1b95e118e5d37a0
DavidChenGoogle <dxchen@google.com>
2014-12-01 15:43:53 +0000

    Hooked up answers from a group appropriately, yay!

--- odkcollect/src/main/java/org/odk/collect/android/widgets2/group/WidgetGroup.java
+++ odkcollect/src/main/java/org/odk/collect/android/widgets2/group/WidgetGroup.java
@@ -3,6 +3,10 @@ package org.odk.collect.android.widgets2.group;
 import android.view.ViewManager;
 import android.view.ViewParent;
 
+import org.odk.collect.android.widgets2.common.TypedWidget;
+
+import java.util.List;
+
 /**
  * A group of {@link org.odk.collect.android.widgets2.common.TypedWidget}s that should be displayed
  * together.
@@ -11,4 +15,7 @@ import android.view.ViewParent;
  * though this is only loosely enforced by {@link WidgetGroup} extending {@link ViewParent} and
  * {@link ViewManager}.
  */
-public interface WidgetGroup extends ViewParent, ViewManager {}
+public interface WidgetGroup extends ViewParent, ViewManager {
+
+    List<TypedWidget<?>> getWidgets();
+}
