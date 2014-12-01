99ffdfc030969ad672f51a571590cf488ea5f866
Nick Fortescue <nfortescue@google.com>
2014-12-01 17:22:19 +0000

    RPCs for locations that may or may not work. They were written before the server implementation was ready.

--- app/src/main/java/org/msf/records/net/Server.java
+++ app/src/main/java/org/msf/records/net/Server.java
@@ -4,6 +4,7 @@ import android.support.annotation.Nullable;
 
 import com.android.volley.Response;
 
+import org.msf.records.net.model.Location;
 import org.msf.records.net.model.NewUser;
 import org.msf.records.net.model.Patient;
 import org.msf.records.net.model.User;
@@ -109,6 +110,51 @@ public interface Server {
     public void listUsers(@Nullable String filterQueryTerm,
                           Response.Listener<List<User>> userListener,
                           Response.ErrorListener errorListener, String logTag);
+
+    /**
+     * Add a new location to the server.
+     *
+     * @param location uuid must not be set, parent_uuid must be set, and the names map must have a
+     *                 name for at least one locale.
+     * @param locationListener the listener to be informed of the newly added location
+     * @param errorListener listener to be informed of any errors
+     * @param logTag a unique argument for tagging logs to aid debugging
+     */
+    public void addLocation(Location location,
+                            final Response.Listener<Location> locationListener,
+                            final Response.ErrorListener errorListener,
+                            final String logTag);
+
+    /**
+     * Update the names for a location on the server.
+     *
+     * @param location the location, only uuid and new locale names for the location will be used,
+     *                 but ideally the other arguments should be correct
+     * @param locationListener the listener to be informed of the newly added location
+     * @param errorListener listener to be informed of any errors
+     * @param logTag a unique argument for tagging logs to aid debugging
+     */
+    public void updateLocation(Location location,
+                               final Response.Listener<Location> locationListener,
+                               final Response.ErrorListener errorListener,
+                               final String logTag);
+
+    /**
+     * Delete a given location from the server. The location should not be the EMC location or
+     * one of the zones - just a client added location, tent or bed.
+     */
+    public void deleteLocation(String locationUuid,
+                               final Response.ErrorListener errorListener,
+                               final String logTag);
+
+    /**
+     * List all locations.
+     *
+     * @param logTag a unique argument for tagging logs to aid debugging
+     */
+    public void listLocations(Response.Listener<List<Location>> locationListener,
+                              Response.ErrorListener errorListener,
+                              String logTag);
 
     /**
      * Cancel all requests associated with the given tag.
