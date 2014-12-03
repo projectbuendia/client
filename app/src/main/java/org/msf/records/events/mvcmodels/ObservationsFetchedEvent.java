eca1147e7bb0dcede4cf8242d2854be4672d09cc
DavidChenGoogle <dxchen@google.com>
2014-12-03 16:30:11 +0000

    Introduced MVC-ish controllers, added abstractions around syncing, and created a skeleton for MVC-ish models.
    Originally, we wanted to be able to implement the following: when a new observation is added, the patient chart should be updated. Upon investigation, this proved to be difficult:
    
     * PatientChartActivity handles the activity result from ODK and posts it to the server.
     * PatientChartFragment displays the observation grid.
    
    We needed PatientChartFragment to wait until the posted succeeds, meaning we need some kind of shared state between the activity and fragment. We could have done this with just an event, but where's the fun in that?

---
+++ app/src/main/java/org/msf/records/events/mvcmodels/ObservationsFetchedEvent.java
@@ -0,0 +1,7 @@
+package org.msf.records.events.mvcmodels;
+
+/**
+ * Created by dxchen on 12/3/14.
+ */
+public class ObservationsFetchedEvent {
+}
---
+++ app/src/main/java/org/msf/records/events/sync/SyncStartedEvent.java
@@ -0,0 +1,6 @@
+package org.msf.records.events.sync;
+
+/**
+ * An event bus event indicating that a sync has started.
+ */
+public class SyncStartedEvent {}
