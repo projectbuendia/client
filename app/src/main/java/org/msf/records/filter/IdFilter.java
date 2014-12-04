27ccb24dd37aa19caf383acea9380b6034324a60
Lunarsong <shanee@google.com>
2014-12-04 21:31:02 +0000

    - Added colors to patient chart items. - Added comment so we know where to add background color for patients in patient list.

--- app/src/main/java/org/msf/records/filter/IdFilter.java
+++ app/src/main/java/org/msf/records/filter/IdFilter.java
@@ -8,11 +8,11 @@ import org.msf.records.sync.PatientProviderContract;
 public class IdFilter implements SimpleSelectionFilter {
     @Override
     public String getSelectionString() {
-        return PatientProviderContract.PatientColumns._ID + "=?";
+        return PatientProviderContract.PatientColumns._ID + " LIKE ?";
     }
 
     @Override
     public String[] getSelectionArgs(CharSequence constraint) {
-        return new String[] { constraint.toString() };
+        return new String[] { "%" + constraint.toString() + "%" };
     }
 }
