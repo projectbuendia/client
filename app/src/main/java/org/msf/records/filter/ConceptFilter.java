56e92d48e8057ebf266ff9b87d24822cfb129e85
akalachman <akalachman@gmail.com>
2014-12-07 16:38:15 +0000

    Fixes concept filter/pregnant filter to not allow rows in which the concept previously had the given value

--- app/src/main/java/org/msf/records/filter/ConceptFilter.java
+++ app/src/main/java/org/msf/records/filter/ConceptFilter.java
@@ -1,5 +1,7 @@
 package org.msf.records.filter;
 
+import android.util.Log;
+
 import org.msf.records.sync.ChartProviderContract;
 import org.msf.records.sync.PatientDatabase;
 import org.msf.records.sync.PatientProviderContract;
@@ -13,27 +15,32 @@ public class ConceptFilter implements SimpleSelectionFilter {
     // the latest observation.
     private static final String CONCEPT_SUBQUERY =
         PatientProviderContract.PatientColumns.COLUMN_NAME_UUID +
-        " IN (SELECT " + ChartProviderContract.ChartColumns.PATIENT_UUID +
+        " IN (SELECT patient_uuid FROM " +
+         "(SELECT obs." + ChartProviderContract.ChartColumns.PATIENT_UUID + " as patient_uuid," +
+            "obs." + ChartProviderContract.ChartColumns.VALUE + " as concept_value" +
             " FROM " +
             PatientDatabase.OBSERVATIONS_TABLE_NAME + " obs " +
 
             " INNER JOIN " +
 
-                "(SELECT " + ChartProviderContract.ChartColumns.CONCEPT_UUID +
+                "(SELECT " + ChartProviderContract.ChartColumns.CONCEPT_UUID + "," +
+                ChartProviderContract.ChartColumns.PATIENT_UUID +
                 ", MAX(" + ChartProviderContract.ChartColumns.ENCOUNTER_TIME + ") AS maxtime" +
                 " FROM " + PatientDatabase.OBSERVATIONS_TABLE_NAME +
-                " GROUP BY " + ChartProviderContract.ChartColumns.CONCEPT_UUID + "," +
-                    ChartProviderContract.ChartColumns.PATIENT_UUID +
+                " GROUP BY " + ChartProviderContract.ChartColumns.PATIENT_UUID + "," +
+                    ChartProviderContract.ChartColumns.CONCEPT_UUID +
                 ") maxs " +
 
                 "ON obs." + ChartProviderContract.ChartColumns.ENCOUNTER_TIME +
                     " = maxs.maxtime AND " +
                 "obs." + ChartProviderContract.ChartColumns.CONCEPT_UUID + "=maxs."
-                    + ChartProviderContract.ChartColumns.CONCEPT_UUID +
+                    + ChartProviderContract.ChartColumns.CONCEPT_UUID + " AND " +
+                "obs." + ChartProviderContract.ChartColumns.PATIENT_UUID + "=maxs."
+                    + ChartProviderContract.ChartColumns.PATIENT_UUID +
 
-                " WHERE obs." + ChartProviderContract.ChartColumns.CONCEPT_UUID + "=? AND " +
-                "obs." + ChartProviderContract.ChartColumns.VALUE + "=?" +
-                " ORDER BY obs." + ChartProviderContract.ChartColumns.PATIENT_UUID + ")";
+                " WHERE obs." + ChartProviderContract.ChartColumns.CONCEPT_UUID + "=?" +
+                " ORDER BY obs." + ChartProviderContract.ChartColumns.PATIENT_UUID + ")" +
+                " WHERE concept_value=?)";
 
     private final String mConceptUuid;
     private final String mConceptValueUuid;
