58fe748fcea95177ecf43e77d85bb757115e32f1
akalachman <akalachman@gmail.com>
2014-12-07 17:50:02 +0000

    Removes group headers from RoundActivity

--- app/src/main/java/org/msf/records/ui/RoundActivity.java
+++ app/src/main/java/org/msf/records/ui/RoundActivity.java
@@ -20,7 +20,7 @@ public class RoundActivity extends PatientSearchActivity {
 
     private int mLocationPatientCount;
 
-    private PatientListFragment mFragment;
+    private SingleLocationPatientListFragment mFragment;
     private SimpleSelectionFilter mFilter;
 
     public static final String LOCATION_NAME_KEY = "location_name";
@@ -58,7 +58,7 @@ public class RoundActivity extends PatientSearchActivity {
         inflater.inflate(R.menu.main, menu);
 
         // TODO(akalachman): Move this back to onCreate when I figure out why it needs to be here.
-        mFragment = (PatientListFragment)getSupportFragmentManager()
+        mFragment = (SingleLocationPatientListFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.round_patient_list);
         mFragment.filterBy(mFilter);
 
