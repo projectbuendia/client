e9199e49117d4b02d55709e3960807a27445cc6c
Ryan Lothian <x>
2014-12-10 13:37:14 +0000

    Fix my breakage to Location.
    I hadn't realised that other parts of the app were constructing
    it by reflection. This class is implicitly our network API
    so modifying it causes problems. Added a warning comment.

--- app/src/main/java/org/msf/records/model/LocalizedString.java
+++ app/src/main/java/org/msf/records/model/LocalizedString.java
@@ -3,35 +3,43 @@ package org.msf.records.model;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableMap;
 
 public final class LocalizedString {
 	private final ImmutableMap<String, String> mTranslationByLocale;
-	
+
 	private LocalizedString(Builder builder) {
 		mTranslationByLocale = ImmutableMap.copyOf(builder.mTranslationByLocale);
+		Preconditions.checkNotNull(mTranslationByLocale, "mTranslationByLocale");
 	}
-	
+
 	public boolean isEmpty() {
 		return mTranslationByLocale.isEmpty();
 	}
-	
+
 	public ImmutableCollection<String> getLocales() {
 		return mTranslationByLocale.keySet();
 	}
-	
+
 	public String getTranslationForLocale(String locale) {
 		return mTranslationByLocale.get(locale);
 	}
-	
+
+	public Map<String, String> asMap() {
+		return mTranslationByLocale;
+	}
+
     public static final class Builder {
     	private final Map<String, String> mTranslationByLocale = new HashMap<>();
-    	
+
     	public void addTranslation(String locale, String translation) {
+    		Preconditions.checkNotNull(locale);
+    		Preconditions.checkNotNull(translation);
     		mTranslationByLocale.put(locale, translation);
     	}
-    	
+
     	public LocalizedString build() {
     		return new LocalizedString(this);
     	}
