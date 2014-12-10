d1edff58b6262575f14f6b1219177ccbb45b4eb9
Nick Fortescue <nfortescue@google.com>
2014-12-10 18:45:12 +0000

    Removed ControllableFragment as it never got used.

--- app/src/main/java/org/msf/records/controllers/BaseController.java
+++ app/src/main/java/org/msf/records/controllers/BaseController.java
@@ -3,34 +3,24 @@ package org.msf.records.controllers;
 import android.content.Intent;
 
 import org.msf.records.ui.ControllableActivity;
-import org.msf.records.ui.ControllableFragment;
 
 /**
  * A convenient abstract controller that can control both {@link ControllableActivity} and
  * {@link android.app.Fragment} instances.
  */
-public abstract class BaseController implements ActivityController, FragmentController {
+public abstract class BaseController implements ActivityController {
 
     public void register(ControllableActivity activity) {
         activity.registerController(this);
     }
 
-    public void register(ControllableFragment fragment) {
-        fragment.registerController(this);
-    }
 
     public void unregister(ControllableActivity activity) {
         activity.unregisterController(this);
     }
 
-    public void unregister(ControllableFragment fragment) {
-        fragment.unregisterController(this);
-    }
 
     @Override
     public void onActivityResult(
             ControllableActivity activity, int requestCode, int resultCode, Intent data) {}
-
-    @Override
-    public void onFragmentResume() {}
 }
