14f9c0d0c154644e30efee7c5ca174863855fc76
Ryan Lothian <x>
2014-12-11 16:56:00 +0000

    Refactor LocationManager and fix multiple bugs.
    - Thread safety bug: synchronizing on the wrong objects
    - Thread safety bug: calling AsyncTask.execute on a background thread
    - Bug: you had to open PatientListFragment before the tent list would
      load. The fix was to make sure we register a sync account before we
      sync.
    - Use dagger to inject a LocationManager singleton instead of creating
      instances in activities/fragments.

--- app/src/main/java/org/msf/records/events/location/LocationsSyncedEvent.java
+++ app/src/main/java/org/msf/records/events/location/LocationsSyncedEvent.java
@@ -4,7 +4,7 @@ package org.msf.records.events.location;
  * An event bus event that indicates that locations have successfully been synced to the client
  * database (but have not necessarily been applied to the LocationTree model).
  */
-public class LocationsSyncedEvent {
 
+public class LocationsSyncedEvent {
     public LocationsSyncedEvent() {}
 }
