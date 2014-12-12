50d2a1fd7f15c00901415971c39d9884ebbbbf46
DavidChenGoogle <dxchen@google.com>
2014-12-12 11:09:16 +0000

    Implemented patient creation

--- app/src/main/java/org/msf/records/ui/patientcreation/PatientCreationController.java
+++ app/src/main/java/org/msf/records/ui/patientcreation/PatientCreationController.java
@@ -16,11 +16,14 @@ import org.msf.records.location.LocationTree;
 import org.msf.records.location.LocationTree.LocationSubtree;
 import org.msf.records.model.Zone;
 import org.msf.records.net.OpenMrsServer;
+import org.msf.records.net.Server;
 import org.msf.records.net.model.Patient;
 import org.msf.records.utils.EventBusRegistrationInterface;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Nullable;
@@ -55,8 +58,8 @@ final class PatientCreationController {
 
         void onValidationError(int field, String message);
 
-        void onCreateFailed();
-        void onCreateSucceeded();
+        void onCreateFailed(VolleyError error);
+        void onCreateSucceeded(Patient response);
     }
 
 	private final Ui mUi;
@@ -72,30 +75,74 @@ final class PatientCreationController {
     }
 
     public void createPatient(
-            CharSequence id,
-            CharSequence givenName,
-            CharSequence familyName,
-            CharSequence age,
-            int ageUnits,
-            int sex) {
-
+            String id, String givenName, String familyName, String age, int ageUnits, int sex) {
         // Validate the input.
+        boolean hasValidationErrors = false;
         if (id == null || id.equals("")) {
-            mUi.onValidationError();
+            mUi.onValidationError(Ui.FIELD_ID, "ID must not be empty.");
+            hasValidationErrors = true;
+        }
+        if (givenName == null || givenName.equals("")) {
+            mUi.onValidationError(Ui.FIELD_GIVEN_NAME, "Given name must not be empty.");
+            hasValidationErrors = true;
+        }
+        if (familyName == null || familyName.equals("")) {
+            mUi.onValidationError(Ui.FIELD_FAMILY_NAME, "Family name must not be empty.");
+            hasValidationErrors = true;
+        }
+        if (age == null || age.equals("")) {
+            mUi.onValidationError(Ui.FIELD_AGE, "Age must not be empty.");
+            hasValidationErrors = true;
+        }
+        int ageInt = 0;
+        try {
+            ageInt = Integer.parseInt(age);
+        } catch (NumberFormatException e) {
+            mUi.onValidationError(Ui.FIELD_AGE, "Age must be an integer.");
+            hasValidationErrors = true;
+        }
+        if (ageInt < 0) {
+            mUi.onValidationError(Ui.FIELD_AGE, "Age must be non-negative.");
+            hasValidationErrors = true;
+        }
+        if (ageUnits != AGE_YEARS && ageUnits != AGE_MONTHS) {
+            mUi.onValidationError(Ui.FIELD_AGE_UNITS, "Age units must be specified.");
+            hasValidationErrors = true;
+        }
+        if (sex != SEX_MALE && sex != SEX_FEMALE) {
+            mUi.onValidationError(Ui.FIELD_SEX, "Sex must be specified.");
+            hasValidationErrors = true;
         }
 
-        mServer.addPatient(null, mAddPatientListener, mAddPatientListener, TAG);
+        if (hasValidationErrors) {
+            return;
+        }
+
+        Map<String, String> patientArguments = new HashMap<>();
+        patientArguments.put(Server.PATIENT_ID_KEY, id);
+        patientArguments.put(Server.PATIENT_GIVEN_NAME_KEY, givenName);
+        patientArguments.put(Server.PATIENT_FAMILY_NAME_KEY, familyName);
+
+        // TODO(dxchen,nfortescue): Add patient should support age.
+
+        patientArguments.put(Server.PATIENT_GENDER_KEY, sex == SEX_MALE ? "M" : "F");
+
+        mServer.addPatient(patientArguments, mAddPatientListener, mAddPatientListener, TAG);
     }
 
     private final class AddPatientListener
             implements Response.Listener<Patient>, Response.ErrorListener {
 
         @Override public void onResponse(Patient response) {
-            mUi.onCreateSucceeded();
+            // TODO(dxchen): Write the newly-added patient to the model directly so that we do not
+            // have to do a sync afterwards to see the patient. Make sure not to call the UI
+            // callback until the model add has finished.
+
+            mUi.onCreateSucceeded(response);
         }
 
         @Override public void onErrorResponse(VolleyError error) {
-            mUi.onCreateFailed();
+            mUi.onCreateFailed(error);
         }
     }
 }
