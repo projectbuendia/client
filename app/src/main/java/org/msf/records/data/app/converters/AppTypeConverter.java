0a90f180b7f2df4ada48e32898b1a7bcefeef45d
Ryan Lothian <x>
2014-12-15 21:20:18 +0000

    Tidy up some interfaces related to TypedCursor.
    * Prefer interfaces to abstract classes (as Java doesn't
      allow multiple inheritance).
    * Remove unused type extends AppTypeBase requirements from
      some classes/interfaces.

--- app/src/main/java/org/msf/records/data/app/converters/AppTypeConverter.java
+++ app/src/main/java/org/msf/records/data/app/converters/AppTypeConverter.java
@@ -2,13 +2,11 @@ package org.msf.records.data.app.converters;
 
 import android.database.Cursor;
 
-import org.msf.records.data.app.AppTypeBase;
-
 /**
  * An interface for a converter that converts between model data types and Android database
  * abstractions.
  */
-public interface AppTypeConverter<T extends AppTypeBase> {
+public interface AppTypeConverter<T> {
 
     /**
      * Converts the current position in a {@link Cursor} to a model data type.
