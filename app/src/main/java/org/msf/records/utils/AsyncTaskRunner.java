ae9c71a1eb6637a9c9cb7121b2da610675fc69d2
akalachman <akalachman@gmail.com>
2015-01-13 13:54:42 -0800

    Removes main thread restriction on AsyncTasks and instead automatically executes AsyncTasks from the main thread

--- app/src/main/java/org/msf/records/utils/AsyncTaskRunner.java
+++ app/src/main/java/org/msf/records/utils/AsyncTaskRunner.java
@@ -1,6 +1,8 @@
 package org.msf.records.utils;
 
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 
 /**
  * Runs async tasks in a unit-testable way.
@@ -11,10 +13,17 @@ public interface AsyncTaskRunner {
         @Override
         @SafeVarargs
         public final <Params, Progress, Result> void runTask(
-                AsyncTask<Params, Progress, Result> asyncTask,
-                Params... params) {
-            ThreadUtils.checkOnMainThread();
-            asyncTask.execute(params);
+                final AsyncTask<Params, Progress, Result> asyncTask,
+                final Params... params) {
+            // Force the AsyncTask to start from the main thread (since using any other thread will
+            // result in an exception, anyway).
+            Handler mainHandler = new Handler(Looper.getMainLooper());
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    asyncTask.execute(params);
+                }
+            });
         }
     };
 
@@ -22,5 +31,4 @@ public interface AsyncTaskRunner {
     public <Params, Progress, Result> void runTask(
             AsyncTask<Params, Progress, Result> asyncTask,
             Params... params);
-
 }
