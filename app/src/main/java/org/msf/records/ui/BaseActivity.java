630e2ffc8f0bca12f3abd46ff5600c3360315366
Ka-Ping Yee <ping@zesty.ca>
2015-01-23 10:28:32 +0000

    Don't hide status bar.

--- app/src/main/java/org/msf/records/ui/BaseActivity.java
+++ app/src/main/java/org/msf/records/ui/BaseActivity.java
@@ -237,11 +237,8 @@ public abstract class BaseActivity extends FragmentActivity {
                 View decor = getWindow().getDecorView();
                 if (decor != null) {
                     decor.setSystemUiVisibility(
-                            // View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                    // | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
                                     | View.SYSTEM_UI_FLAG_IMMERSIVE
                                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     );
