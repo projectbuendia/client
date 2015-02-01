ed8634fe9da7f5e64e4ffe7e24f87283abb86343
Ka-Ping Yee <ping@zesty.ca>
2015-02-01 03:24:55 +0000

    Gracefully handle very long integers in patient IDs.

--- app/src/androidTest/java/org/msf/records/utils/UtilsTest.java
+++ app/src/androidTest/java/org/msf/records/utils/UtilsTest.java
@@ -9,11 +9,15 @@ import java.util.Arrays;
 public class UtilsTest extends TestCase {
     /** Tests Utils.alphanumericComparator. */
     public void testAlphanumericComparator() throws Exception {
-        String[] elements = {"b1", "a11a", "a11", "a2", "a2b", "a02b", "a2a", "a1"};
+        String[] elements = {
+                "b1", "a11a", "a11", "a2", "a2b", "a02b", "a2a", "a1",
+                "b7465829459273654782634", "b7465829459273654782633"};
         String[] sorted = elements.clone();
         Arrays.sort(sorted, Utils.alphanumericComparator);
         Joiner joiner = Joiner.on("/");
-        String[] expected = {"a1", "a2", "a2a", "a02b", "a2b", "a11", "a11a", "b1"};
+        String[] expected = {
+                "a1", "a2", "a2a", "a02b", "a2b", "a11", "a11a", "b1",
+                "b7465829459273654782633", "b7465829459273654782634"};
         assertEquals(joiner.join(expected), joiner.join(sorted));
     }
 }
