8296565382ee3edefb48fe0fe016549187e8e69c
akalachman <akalachman@gmail.com>
2015-02-16 14:44:00 +0000

    Creates and utilizes new function to wait for a matched view to appear within a specified timeout, and removes previous attempt at creating such a function

--- app/src/androidTest/java/org/msf/records/ui/matchers/ViewMatchers.java
+++ app/src/androidTest/java/org/msf/records/ui/matchers/ViewMatchers.java
@@ -3,65 +3,13 @@ package org.msf.records.ui.matchers;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 
-import com.google.android.apps.common.testing.ui.espresso.ViewAssertion;
-
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.mockito.ArgumentMatcher;
-import org.msf.records.utils.Logger;
 
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 
 public class ViewMatchers {
-    private static final Logger LOG = Logger.create();
-    private static final int CHECK_INTERVAL_MILLIS = 200;
-
-    /**
-     * Periodically applies the matcher, asserting that the matcher is matched at some point before
-     * the specified timeout is reached. This may be useful in cases where views are being updated
-     * quickly (but asynchronously) and no event bus event is available for use with
-     * {@link org.msf.records.ui.sync.EventBusIdlingResource}.
-     */
-    public static ViewAssertion matchesWithin(final Matcher<View> matcher, final long timeoutMs) {
-        return matches(within(matcher, timeoutMs));
-    }
-
-    /**
-     * Provides a {@link Matcher} that wraps another {@link Matcher}, periodically applying it and
-     * returning true if the {@link View} is matched within a specified period of time.
-     */
-    private static ArgumentMatcher<View> within(
-            final Matcher<View> matcher, final long timeoutMs) {
-        return new ArgumentMatcher<View>() {
-
-            @Override
-            public boolean matches(final Object o) {
-                View v = (View)o;
-                LOG.v("Before matching loop");
-                long timeoutTime = System.currentTimeMillis() + timeoutMs;
-                while (timeoutTime > System.currentTimeMillis()) {
-                    LOG.v("In matching loop");
-                    long endTime = System.currentTimeMillis() + CHECK_INTERVAL_MILLIS;
-                    // Check internal matcher.
-                    if (matcher.matches(v)) {
-                        return true;
-                    }
-                    // Wait until the next check interval.
-                    if (endTime > System.currentTimeMillis()) {
-                        try {
-                            Thread.sleep(endTime - System.currentTimeMillis());
-                        } catch (InterruptedException e) {
-                            LOG.w("Unable to sleep");
-                        }
-                    }
-                }
-
-                // Timeout.
-                LOG.v("returning false");
-                return false;
-            }
-        };
-    }
 
     /**
      * Provides a {@link Matcher} that matches any view in the given row, where each row has a
