37b57858ad65fccf0818387269987df36ef8c51a
Nick Fortescue <nfortescue@google.com>
2015-03-27 10:43:39 +0000

    Modified SQL cipher comment

--- app/src/main/java/org/msf/records/sync/PatientDatabase.java
+++ app/src/main/java/org/msf/records/sync/PatientDatabase.java
@@ -25,7 +25,7 @@ public class PatientDatabase extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 15;
     /** Filename for SQLite file. */
     public static final String DATABASE_NAME = "patientscipher.db";
-    /**
+    /*
      * This deserves a brief comment on the threat model. Patient data encrypted by a hardcoded key
      * might seems like security by obscurity. It is.
      *
