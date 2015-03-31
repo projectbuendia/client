63fe1cbbb94cf937ce0433b6576732e5c040fcd8
Ka-Ping Yee <ping@zesty.ca>
2015-03-31 13:49:03 +0100

    Use common demo patient and navigation across tests.
    Fixes unreliable LocationSelectionActivityTest (existence of a necessary
    demo patient used to depend on test execution order).

--- app/src/androidTest/java/org/msf/records/ui/FunctionalTestCase.java
+++ app/src/androidTest/java/org/msf/records/ui/FunctionalTestCase.java
@@ -12,6 +12,8 @@
 package org.msf.records.ui;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.test.ActivityInstrumentationTestCase2;
@@ -19,33 +21,52 @@ import android.view.View;
 
 import com.google.android.apps.common.testing.testrunner.ActivityLifecycleMonitorRegistry;
 import com.google.android.apps.common.testing.testrunner.Stage;
+import com.google.android.apps.common.testing.ui.espresso.Espresso;
 import com.google.android.apps.common.testing.ui.espresso.IdlingPolicies;
 import com.google.android.apps.common.testing.ui.espresso.NoActivityResumedException;
+import com.google.common.base.Optional;
 import com.google.common.collect.Iterables;
 import com.squareup.spoon.Spoon;
-import com.google.android.apps.common.testing.ui.espresso.Espresso;
 
 import org.hamcrest.Matcher;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.Period;
+import org.msf.records.R;
+import org.msf.records.data.app.AppPatient;
+import org.msf.records.data.app.AppPatientDelta;
+import org.msf.records.events.data.SingleItemCreatedEvent;
 import org.msf.records.events.sync.SyncFinishedEvent;
 import org.msf.records.events.sync.SyncStartedEvent;
 import org.msf.records.events.sync.SyncSucceededEvent;
 import org.msf.records.events.user.KnownUsersLoadedEvent;
+import org.msf.records.net.model.Patient;
 import org.msf.records.ui.sync.EventBusIdlingResource;
 import org.msf.records.ui.userlogin.UserLoginActivity;
-import org.msf.records.utils.Logger;
 import org.msf.records.utils.EventBusRegistrationInterface;
 import org.msf.records.utils.EventBusWrapper;
+import org.msf.records.utils.Logger;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import de.greenrobot.event.EventBus;
 
-import java.util.concurrent.TimeUnit;
-
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.pressBack;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.RootMatchers.isDialog;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withParent;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.msf.records.ui.matchers.AppPatientMatchers.isPatientWithId;
 
 /**
  * Base class for functional tests that sets timeouts to be permissive, optionally logs in as a
@@ -58,6 +79,10 @@ public class FunctionalTestCase extends ActivityInstrumentationTestCase2<UserLog
     private boolean mWaitForUserSync = true;
 
     protected EventBusRegistrationInterface mEventBus;
+    // For now, we create a new demo patient for tests using the real new
+    // patient UI on each test run (see {@link #inUserLoginInitDemoPatient()}).
+    // TODO/robustness: Use externally preloaded demo data instead.
+    protected static String sDemoPatientId = null;
 
     public FunctionalTestCase() {
         super(UserLoginActivity.class);
@@ -150,6 +175,7 @@ public class FunctionalTestCase extends ActivityInstrumentationTestCase2<UserLog
      * <p>Warning: This function will not work properly in setUp() as the current activity won't
      * be available. If you need to call this function during setUp(), use
      * {@link #waitForProgressFragment(ProgressFragment)}.
+     * TODO/robustness: Investigate why the current activity isn't available during setUp().
      */
     protected void waitForProgressFragment() {
         Activity activity;
@@ -217,6 +243,253 @@ public class FunctionalTestCase extends ActivityInstrumentationTestCase2<UserLog
         }
     }
 
+    /**
+     * Adds a new patient using the new patient form.  Assumes that the UI is
+     * in the location selection activity, and leaves the UI in the same
+     * activity.  Note: this function will not work during {@link #setUp()}
+     * as it relies on {@link #waitForProgressFragment()}.
+     * @param delta an AppPatientDelta containing the data for the new patient;
+     *     use Optional.absent() to leave fields unset
+     * @param locationName the name of a location to assign to the new patient,
+     *     or null to leave unset (assumes this name is unique among locations)
+     */
+    protected void inLocationSelectionAddNewPatient(AppPatientDelta delta, String locationName) {
+        LOG.i("Adding patient: %s (location %s)",
+                delta.toContentValues().toString(), locationName);
+
+        onView(withId(R.id.action_add)).perform(click());
+        onView(withText("New Patient")).check(matches(isDisplayed()));
+        if (delta.id.isPresent()) {
+            onView(withId(R.id.patient_creation_text_patient_id))
+                    .perform(typeText(delta.id.get()));
+        }
+        if (delta.givenName.isPresent()) {
+            onView(withId(R.id.patient_creation_text_patient_given_name))
+                    .perform(typeText(delta.givenName.get()));
+        }
+        if (delta.familyName.isPresent()) {
+            onView(withId(R.id.patient_creation_text_patient_family_name))
+                    .perform(typeText(delta.familyName.get()));
+        }
+        if (delta.birthdate.isPresent()) {
+            Period age = new Period(delta.birthdate.get().toLocalDate(), LocalDate.now());
+            if (age.getYears() < 1) {
+                onView(withId(R.id.patient_creation_text_age))
+                        .perform(typeText(Integer.toString(age.getMonths())));
+                onView(withId(R.id.patient_creation_radiogroup_age_units_months)).perform(click());
+            } else {
+                onView(withId(R.id.patient_creation_text_age))
+                        .perform(typeText(Integer.toString(age.getYears())));
+                onView(withId(R.id.patient_creation_radiogroup_age_units_years)).perform(click());
+            }
+        }
+        if (delta.gender.isPresent()) {
+            if (delta.gender.get() == AppPatient.GENDER_MALE) {
+                onView(withId(R.id.patient_creation_radiogroup_age_sex_male)).perform(click());
+            } else if (delta.gender.get() == AppPatient.GENDER_FEMALE) {
+                onView(withId(R.id.patient_creation_radiogroup_age_sex_female)).perform(click());
+            }
+        }
+        if (delta.admissionDate.isPresent()) {
+            // TODO/completeness: Support admission date in addNewPatient().
+            // The following code is broken -- hopefully fixed by Espresso 2.0.
+            // onView(withId(R.id.patient_creation_admission_date)).perform(click());
+            // selectDateFromDatePicker(mDemoPatient.admissionDate.get());
+        }
+        if (delta.firstSymptomDate.isPresent()) {
+            // TODO/completeness: Support first symptoms date in addNewPatient().
+            // The following code is broken -- hopefully fixed by Espresso 2.0.
+            // onView(withId(R.id.patient_creation_symptoms_onset_date)).perform(click());
+            // selectDateFromDatePicker(mDemoPatient.firstSymptomDate.get());
+        }
+        if (delta.assignedLocationUuid.isPresent()) {
+            // TODO/completeness: Support assigned location in addNewPatient().
+            // A little tricky as we need to select by UUID.
+            // onView(withId(R.id.patient_creation_button_change_location)).perform(click());
+        }
+        if (locationName != null) {
+            onView(withId(R.id.patient_creation_button_change_location)).perform(click());
+            onView(withText(locationName)).perform(click());
+        }
+
+        EventBusIdlingResource<SingleItemCreatedEvent<AppPatient>> resource =
+                new EventBusIdlingResource<>(UUID.randomUUID().toString(), mEventBus);
+
+        onView(withId(R.id.patient_creation_button_create)).perform(click());
+        Espresso.registerIdlingResources(resource); // wait for patient to be created
+    }
+
+    // Broken, but hopefully fixed in Espresso 2.0.
+    private void selectDateFromDatePickerDialog(DateTime dateTime) {
+        onView(withText("Set"))
+                .inRoot(isDialog())
+                .perform(click());
+    }
+
+    protected void selectDateFromDatePicker(
+            @Nullable String year,
+            @Nullable String monthOfYear,
+            @Nullable String dayOfMonth) {
+        LOG.e("Year: %s, Month: %s, Day: %s", year, monthOfYear, dayOfMonth);
+
+        if (year != null) {
+            setDateSpinner("year", year);
+        }
+        if (monthOfYear != null) {
+            setDateSpinner("month", monthOfYear);
+        }
+        if (dayOfMonth != null) {
+            setDateSpinner("day", dayOfMonth);
+        }
+    }
+
+    protected void selectDateFromDatePicker(DateTime dateTime) {
+        String year = dateTime.toString("yyyy");
+        String monthOfYear = dateTime.toString("MMM");
+        String dayOfMonth = dateTime.toString("dd");
+
+        selectDateFromDatePicker(year, monthOfYear, dayOfMonth);
+    }
+
+    // Broken, but hopefully fixed in Espresso 2.0.
+    protected void setDateSpinner(String spinnerName, String value) {
+        int numberPickerId =
+                Resources.getSystem().getIdentifier("numberpicker_input", "id", "android");
+        int spinnerId =
+                Resources.getSystem().getIdentifier(spinnerName, "id", "android");
+        LOG.i("%s: %s", spinnerName, value);
+        LOG.i("numberPickerId: %d", numberPickerId);
+        LOG.i("spinnerId: %d", spinnerId);
+        onView(allOf(withId(numberPickerId), withParent(withId(spinnerId))))
+                .check(matches(isDisplayed()))
+                .perform(typeText(value));
+    }
+
+    /**
+     * Ensures that a demo patient exists, creating one if necessary.  Assumes
+     * that the UI is in the user login activity, and leaves the UI back in
+     * the user login activity.  Note: this function will not work during
+     * {@link #setUp()} as it relies on {@link #waitForProgressFragment()}.
+     */
+    protected void inUserLoginInitDemoPatient() {
+        if (sDemoPatientId != null) { // demo patient exists and is reusable
+            return;
+        }
+
+        AppPatientDelta delta = new AppPatientDelta();
+        String id = "" + (System.currentTimeMillis() % 100000);
+        delta.id = Optional.of(id);
+        delta.givenName = Optional.of("Given" + id);
+        delta.familyName = Optional.of("Family" + id);
+        delta.firstSymptomDate = Optional.of(LocalDate.now().minusMonths(7));
+        delta.gender = Optional.of(Patient.GENDER_FEMALE);
+        delta.birthdate = Optional.of(DateTime.now().minusYears(12).minusMonths(3));
+        // Setting location within the AppPatientDelta is not yet supported.
+        // delta.assignedLocationUuid = Optional.of(Zone.TRIAGE_ZONE_UUID);
+
+        inUserLoginGoToLocationSelection();
+        inLocationSelectionAddNewPatient(delta, "S1"); // add the patient
+        sDemoPatientId = id; // record ID so future tests can reuse the patient
+        pressBack(); // return to user login activity
+    }
+
+    /**
+     * Prevents the current demo patient from being reused for the next test.
+     * The default behaviour is to reuse the same demo patient for each test;
+     * if a test modifies patient data, it should call this method so that the
+     * next test will use a fresh demo patient.
+     */
+    protected void invalidateDemoPatient() {
+        sDemoPatientId = null;
+    }
+
+    /**
+     * Navigates to the location selection activity from the user login
+     * activity.  Note: this function will not work during {@link #setUp()}
+     * as it uses {@link #waitForProgressFragment()}.
+     */
+    protected void inUserLoginGoToLocationSelection() {
+        onView(withText("Guest User")).perform(click());
+        waitForProgressFragment(); // wait for locations to load
+    }
+
+    /**
+     * Navigates to the location selection activity with a list of all the
+     * patients opened (from tapping the search button).  Assumes that the UI is
+     * in the user login activity.  Note: this function will not work during
+     * {@link #setUp()} as it uses {@link #waitForProgressFragment()}.
+     */
+    protected void inUserLoginGoToPatientList() {
+        inUserLoginGoToLocationSelection();
+        // There may be a small delay before the search button becomes visible;
+        // the button is not displayed while locations are loading.
+        checkViewDisplayedWithin(withId(R.id.action_search), 3000);
+
+        // Tap the search button to open the list of all patients.
+        onView(withId(R.id.action_search)).perform(click());
+    }
+
+    /**
+     * Navigates to the patient chart for the shared demo patient, creating the
+     * demo patient if it doesn't exist yet.  Assumes that the UI is in the
+     * user login activity.  Note: this function will not work during
+     * {@link #setUp()} as it uses {@link #waitForProgressFragment()}.
+     */
+    protected void inUserLoginGoToDemoPatientChart() {
+        inUserLoginInitDemoPatient();
+        inUserLoginGoToPatientList();
+        inPatientListClickPatientWithId(sDemoPatientId);
+    }
+
+    /**
+     * Navigates to the patient creation activity.  Assumes that the UI is
+     * in the user login activity.  Note: this function will not work during
+     * {@link #setUp()} as it uses {@link #waitForProgressFragment()}.
+     */
+    protected void inUserLoginGoToPatientCreation() {
+        inUserLoginGoToLocationSelection();
+        onView(withId(R.id.action_add)).perform(click());
+        onView(withText("New Patient")).check(matches(isDisplayed()));
+    }
+
+
+    /** Checks that the expected zones and tents are shown. */
+    protected void inLocationSelectionCheckZonesAndTentsDisplayed() {
+        // Should be at location selection screen
+        checkViewDisplayedSoon(withText("ALL PRESENT PATIENTS"));
+
+        // Zones and tents should be visible
+        onView(withText("Triage")).check(matches(isDisplayed()));
+        onView(withText("S1")).check(matches(isDisplayed()));
+        onView(withText("S2")).check(matches(isDisplayed()));
+        onView(withText("P1")).check(matches(isDisplayed()));
+        onView(withText("P2")).check(matches(isDisplayed()));
+        onView(withText("C1")).check(matches(isDisplayed()));
+        onView(withText("C2")).check(matches(isDisplayed()));
+        onView(withText("Discharged")).check(matches(isDisplayed()));
+    }
+
+    /** In the location selection activity, click a location tile. */
+    protected void inLocationSelectionClickLocation(String name) {
+        onView(withText(name)).perform(click());
+        waitForProgressFragment(); // Wait for search fragment to load.
+    }
+
+    /** In a patient list, click the first patient. */
+    protected void inPatientListClickFirstPatient() {
+        onData(is(AppPatient.class))
+                .inAdapterView(withId(R.id.fragment_patient_list))
+                .atPosition(0)
+                .perform(click());
+    }
+
+    /** In a patient list, click the patient with a specified ID. */
+    protected void inPatientListClickPatientWithId(String id) {
+        onData(isPatientWithId(equalTo(id)))
+                .inAdapterView(withId(R.id.fragment_patient_list))
+                .atPosition(0)
+                .perform(click());
+    }
 
     private class SyncCounter {
         public int inProgressSyncCount = 0;
--- app/src/androidTest/java/org/msf/records/ui/locationselection/LocationSelectionActivityTest.java
+++ app/src/androidTest/java/org/msf/records/ui/locationselection/LocationSelectionActivityTest.java
@@ -12,18 +12,13 @@
 package org.msf.records.ui.locationselection;
 
 import org.msf.records.R;
-import org.msf.records.data.app.AppPatient;
 import org.msf.records.ui.FunctionalTestCase;
 
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.pressBack;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
-import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.is;
 
 /** Tests for {@link LocationSelectionActivity}. */
 public class LocationSelectionActivityTest extends FunctionalTestCase {
@@ -31,110 +26,68 @@ public class LocationSelectionActivityTest extends FunctionalTestCase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        onView(withText("Guest User")).perform(click());
     }
 
     /** Looks for the expected zones and tents. */
     public void testZonesAndTentsDisplayed() {
-        waitForProgressFragment();
-
-        // Should be at location selection screen
-        checkViewDisplayedSoon(withText("ALL PRESENT PATIENTS"));
-        screenshot("Test Start");
-
-        // Zones and tents should be visible
-        onView(withText("Triage")).check(matches(isDisplayed()));
-        onView(withText("S1")).check(matches(isDisplayed()));
-        onView(withText("S2")).check(matches(isDisplayed()));
-        onView(withText("P1")).check(matches(isDisplayed()));
-        onView(withText("P2")).check(matches(isDisplayed()));
-        onView(withText("C1")).check(matches(isDisplayed()));
-        onView(withText("C2")).check(matches(isDisplayed()));
-        onView(withText("Discharged")).check(matches(isDisplayed()));
+        inUserLoginGoToLocationSelection();
+        inLocationSelectionCheckZonesAndTentsDisplayed();
     }
 
     /** Tests that zones and tents are still displayed after returning from round view. */
     public void testZonesAndTentsDisplayed_afterRoundView() {
-        waitForProgressFragment(); // Wait for initial load.
-
-        // Enter round view and return.
-        onView(withText("S1")).perform(click());
-        waitForProgressFragment(); // Wait for search fragment to load.
+        inUserLoginGoToLocationSelection();
+        inLocationSelectionClickLocation("S1");
         pressBack();
-
-        // Make sure zones and tents are still displayed correctly.
-        testZonesAndTentsDisplayed();
+        inLocationSelectionCheckZonesAndTentsDisplayed();
     }
 
     /** Tests that zones and tents are still displayed after returning from list view. */
     public void testZonesAndTentsDisplayed_afterPatientListView() {
-        waitForProgressFragment(); // Wait for initial load.
-
-        // Enter patient list view and return.
-        onView(withText("ALL PRESENT PATIENTS")).perform(click());
-        waitForProgressFragment(); // Wait for search fragment to load.
+        inUserLoginGoToLocationSelection();
+        inLocationSelectionClickLocation("ALL PRESENT PATIENTS");
         pressBack();
-
-        // Make sure zones and tents are still displayed correctly.
-        testZonesAndTentsDisplayed();
+        inLocationSelectionCheckZonesAndTentsDisplayed();
     }
 
     /** Tests that zones and tents are still displayed after returning from settings view. */
     public void testZonesAndTentsDisplayed_afterSettingsView() {
-        waitForProgressFragment(); // Wait for initial load.
+        inUserLoginGoToLocationSelection();
 
         // Enter settings view and return.
         onView(withText("GU")).perform(click());
         onView(withId(R.id.button_settings)).perform(click());
         pressBack();
-
-        // Make sure zones and tents are still displayed correctly.
-        testZonesAndTentsDisplayed();
+        inLocationSelectionCheckZonesAndTentsDisplayed();
     }
 
     /** Tests that zones and tents are still displayed after returning from chart view. */
     public void testZonesAndTentsDisplayed_afterChartView() {
-        waitForProgressFragment(); // Wait for initial load.
+        inUserLoginInitDemoPatient();
+        inUserLoginGoToLocationSelection();
+        inLocationSelectionClickLocation("S1");
+        inPatientListClickFirstPatient(); // open patient chart
 
-        // Enter a round view.
-        onView(withText("S1")).perform(click());
-        waitForProgressFragment(); // Wait for search fragment to load.
-
-        // Click the first patient.
-        onData(is(AppPatient.class))
-                .inAdapterView(withId(R.id.fragment_patient_list))
-                .atPosition(0)
-                .perform(click());
-
-        pressBack(); // Back to search fragment.
-        pressBack(); // Back to location selection screen.
-
-        // Make sure zones and tents are still displayed correctly.
-        testZonesAndTentsDisplayed();
+        pressBack(); // back to search fragment
+        pressBack(); // back to tent selection screen
+        inLocationSelectionCheckZonesAndTentsDisplayed();
     }
 
     /** Tests that zones and tents are still displayed after changing a patient's location. */
     public void testZonesAndTentsDisplayed_afterPatientLocationChanged() {
-        waitForProgressFragment(); // Wait for initial load.
-
-        // Enter a round view and return.
-        onView(withText("Triage")).perform(click());
-        waitForProgressFragment(); // Wait for search fragment to load.
-
-        // Click the first patient.
-        onData(is(AppPatient.class))
-                .inAdapterView(withId(R.id.fragment_patient_list))
-                .atPosition(0)
-                .perform(click());
+        inUserLoginInitDemoPatient();
+        inUserLoginGoToLocationSelection();
+        inLocationSelectionClickLocation("S1");
+        inPatientListClickFirstPatient(); // open patient chart
 
         // Relocate the patient to C1.
         onView(withId(R.id.action_relocate_patient)).perform(click());
         onView(withText("C1")).perform(click());
 
-        pressBack(); // Back to search fragment.
-        pressBack(); // Back to tent selection screen.
+        pressBack(); // back to search fragment
+        pressBack(); // back to tent selection screen
+        inLocationSelectionCheckZonesAndTentsDisplayed();
 
-        // Make sure zones and tents are still displayed correctly.
-        testZonesAndTentsDisplayed();
+        invalidateDemoPatient(); // don't reuse the relocated patient for future tests
     }
 }
