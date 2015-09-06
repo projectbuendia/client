bdfa8d99d2bf4d8eb4375d909fa9086489254b88
Ka-Ping Yee <ping@zesty.ca>
2015-09-06 18:43:02 +0100

    Rename to grid.html and tidy up use of quotation marks.

--- app/src/main/java/org/projectbuendia/client/ui/chart/GridRenderer.java
+++ app/src/main/java/org/projectbuendia/client/ui/chart/GridRenderer.java
@@ -194,7 +194,7 @@ public class GridRenderer {
             context.put("columns", Lists.newArrayList(mColumnsByStartMillis.values()));
             context.put("nowColumnStart", getColumnContainingTime(DateTime.now()).start);
             context.put("orders", mOrders);
-            return renderTemplate("assets/grid.peb", context);
+            return renderTemplate("assets/grid.html", context);
         }
 
         /** Renders a Pebble template. */
