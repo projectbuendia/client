d49aef4b25c658fb85472a0946282630be37f543
Ka-Ping Yee <ping@zesty.ca>
2015-09-16 10:03:51 -0700

    Fix compilation error.

--- app/src/androidTest/java/org/projectbuendia/client/ui/chart/PatientChartControllerTest.java
+++ app/src/androidTest/java/org/projectbuendia/client/ui/chart/PatientChartControllerTest.java
@@ -12,7 +12,6 @@
 package org.projectbuendia.client.ui.chart;
 
 import android.test.AndroidTestCase;
-import android.util.Pair;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -39,7 +38,6 @@ import org.projectbuendia.client.ui.chart.PatientChartController.MinimalHandler;
 import org.projectbuendia.client.ui.chart.PatientChartController.OdkResultSender;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -56,7 +54,7 @@ public final class PatientChartControllerTest extends AndroidTestCase {
     private static final String PATIENT_ID_1 = "id1";
 
     private static final LocalizedObs OBSERVATION_A =
-        new LocalizedObs(0, 0, "g", "c", "c", "TEXT", "val", "localizedVal");
+        new LocalizedObs(0, 0, "c", "c", "TEXT", "val", "localizedVal");
 
     private PatientChartController mController;
 
@@ -108,9 +106,8 @@ public final class PatientChartControllerTest extends AndroidTestCase {
         mFakeHandler.runUntilEmpty();
         // THEN the controller puts observations on the UI
         verify(mMockUi).updateTilesAndGrid(
-            new ArrayList<Pair<String, String>>(), new HashMap<String, LocalizedObs>(),
-            new ArrayList<Pair<String, String>>(), allObservations,
-            ImmutableList.<Order> of(), null, null);
+            null, new HashMap<String, LocalizedObs>(),
+            allObservations, ImmutableList.<Order> of(), null, null);
         verify(mMockUi).updatePatientVitalsUi(recentObservations, null, null);
     }
 
