3287a5c3c23a65cf5ccc8401f60ca1be713d0faf
Fabian Tamp <fabian.tamp@gmail.com>
2015-10-27 09:24:07 +0800

    Revise note to explicitly discuss Java 8, as referenced in #7

--- app/src/main/java/org/projectbuendia/client/models/AppModel.java
+++ app/src/main/java/org/projectbuendia/client/models/AppModel.java
@@ -111,9 +111,10 @@ public class AppModel {
     public void fetchPatients(CrudEventBus bus, SimpleSelectionFilter filter, String constraint) {
         bus.registerCleanupSubscriber(new CrudEventBusCleanupSubscriber(bus));
         // NOTE: We need to keep the object creation separate from calling #execute() here, because
-        // the type inference breaks otherwise, which throws
+        // the type inference breaks on Java 8 otherwise, which throws
         // `java.lang.ClassCastException: java.lang.Object[] cannot be cast to java.lang.Void[]`.
-        // See http://stackoverflow.com/questions/24136126/fatal-exception-asynctask
+        // See http://stackoverflow.com/questions/24136126/fatal-exception-asynctask and
+        // https://github.com/projectbuendia/client/issues/7
         FetchTypedCursorAsyncTask<Patient> task = new FetchTypedCursorAsyncTask<>(
             Contracts.Patients.CONTENT_URI,
             // The projection must contain an "_id" column for the ListAdapter as well as all
