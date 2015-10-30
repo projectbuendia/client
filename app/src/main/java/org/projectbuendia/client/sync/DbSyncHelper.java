cfc21cb6b56fe8d1179641f6f2a49d0017dd5447
Leonardo Lima de Vasconcellos <leo.lima.web@gmail.com>
2015-10-30 14:09:23 -0200

    Cursors use refactoring. Investigating issue #107

--- app/src/main/java/org/projectbuendia/client/sync/DbSyncHelper.java
+++ app/src/main/java/org/projectbuendia/client/sync/DbSyncHelper.java
@@ -121,17 +121,15 @@ public class DbSyncHelper {
 
         ArrayList<ContentProviderOperation> ops = new ArrayList<>();
         final ContentResolver resolver = App.getInstance().getContentResolver();
-        Cursor c = resolver.query(Contracts.Forms.CONTENT_URI, null, null, null, null);
-        LOG.i("Examining forms: " + c.getCount() + " local, " + cvs.size() + " from server");
-        try {
+
+        try (Cursor c = resolver.query(Contracts.Forms.CONTENT_URI, null, null, null, null)) {
+            LOG.i("Examining forms: " + c.getCount() + " local, " + cvs.size() + " from server");
             while (c.moveToNext()) {
                 String uuid = Utils.getString(c, Contracts.Forms.UUID);
                 Uri uri = Contracts.Forms.CONTENT_URI.buildUpon().appendPath(uuid).build();
                 LOG.i("  - will delete form " + uuid);
                 ops.add(ContentProviderOperation.newDelete(uri).build());
             }
-        } finally {
-            c.close();
         }
 
         for (ContentValues values : cvs.values()) {  // server has a new record
@@ -157,11 +155,9 @@ public class DbSyncHelper {
             cvs.put(patient.id, Patient.fromJson(patient).toContentValues());
         }
 
-        Cursor c = resolver.query(Patients.CONTENT_URI, null, null, null, null);
-        LOG.i("Examining patients: " + c.getCount() + " local, " + cvs.size() + " from server");
-
         List<ContentProviderOperation> ops = new ArrayList<>();
-        try {
+        try (Cursor c = resolver.query(Patients.CONTENT_URI, null, null, null, null)) {
+            LOG.i("Examining patients: " + c.getCount() + " local, " + cvs.size() + " from server");
             while (c.moveToNext()) {
                 String uuid = Utils.getString(c, Patients.UUID);
                 Uri uri = Patients.CONTENT_URI.buildUpon().appendPath(uuid).build();
@@ -182,8 +178,6 @@ public class DbSyncHelper {
                     syncResult.stats.numDeletes++;
                 }
             }
-        } finally {
-            c.close();
         }
 
         for (ContentValues values : cvs.values()) {  // server has a new record
@@ -255,14 +249,14 @@ public class DbSyncHelper {
 
         ArrayList<ContentProviderOperation> ops = new ArrayList<>();
         final ContentResolver resolver = App.getInstance().getContentResolver();
-        Cursor c = resolver.query(Orders.CONTENT_URI, new String[] {
+
+        try (Cursor c = resolver.query(Orders.CONTENT_URI, new String[] {
             Orders.UUID,
             Orders.PATIENT_UUID,
             Orders.INSTRUCTIONS,
             Orders.START_MILLIS,
             Orders.STOP_MILLIS
-        }, null, null, null);
-        try {
+            }, null, null, null)) {
             LOG.i("Examining orders: %d local, %d from server.", c.getCount(), ordersToStore.size());
             // Scan all the locally stored orders, updating the orders we've just received.
             while (c.moveToNext()) {
@@ -285,8 +279,6 @@ public class DbSyncHelper {
                     syncResult.stats.numDeletes++;
                 }
             }
-        } finally {
-            c.close();
         }
 
         // Store all the remaining received orders as new orders.
@@ -357,92 +349,90 @@ public class DbSyncHelper {
         // Get list of all items
         Uri uri = Locations.CONTENT_URI; // Location tree
         Uri namesUri = LocationNames.CONTENT_URI; // Location names
-        Cursor c = contentResolver.query(uri, projection, null, null, null);
-        assert c != null;
-        Cursor namesCur = contentResolver.query(namesUri, namesProjection, null, null, null);
-        assert namesCur != null;
-        LOG.i("Examining locations: %d local, %d from server", c.getCount(), locations.size());
 
         String uuid;
         String parentUuid;
 
         // Build map of location names from the database.
         Map<String, Map<String, String>> dbLocationNames = new HashMap<>();
-        while (namesCur.moveToNext()) {
-            String locationUuid = namesCur.getString(
-                namesCur.getColumnIndex(LocationNames.LOCATION_UUID));
-            String locale = namesCur.getString(
-                namesCur.getColumnIndex(LocationNames.LOCALE));
-            String name = namesCur.getString(
-                namesCur.getColumnIndex(LocationNames.NAME));
-            if (locationUuid == null || locale == null || name == null) continue;
+        try (Cursor namesCur = contentResolver.query(namesUri, namesProjection, null, null, null)) {
+            while (namesCur.moveToNext()) {
+                String locationUuid = namesCur.getString(
+                    namesCur.getColumnIndex(LocationNames.LOCATION_UUID));
+                String locale = namesCur.getString(
+                    namesCur.getColumnIndex(LocationNames.LOCALE));
+                String name = namesCur.getString(
+                    namesCur.getColumnIndex(LocationNames.NAME));
+                if (locationUuid == null || locale == null || name == null) continue;
 
-            if (!dbLocationNames.containsKey(locationUuid)) {
-                dbLocationNames.put(locationUuid, new HashMap<String, String>());
-            }
-
-            dbLocationNames.get(locationUuid).put(locale, name);
-        }
-        namesCur.close();
-
-        // Iterate through the list of locations
-        while (c.moveToNext()) {
-            syncResult.stats.numEntries++;
-
-            uuid = c.getString(c.getColumnIndex(Locations.UUID));
-            parentUuid = c.getString(c.getColumnIndex(Locations.PARENT_UUID));
-
-            JsonLocation location = locationsByUuid.get(uuid);
-            if (location != null) {
-                // Entry exists. Remove from entry map to prevent insert later.
-                locationsByUuid.remove(uuid);
-
-                // Grab the names stored in the database for this location.
-                Map<String, String> locationNames = dbLocationNames.get(uuid);
-
-                // Check to see if the entry needs to be updated
-                Uri existingUri = uri.buildUpon().appendPath(String.valueOf(uuid)).build();
-
-                if (location.parent_uuid != null && !location.parent_uuid.equals(parentUuid)) {
-                    // Update existing record
-                    LOG.i("  - will update location " + uuid);
-                    batch.add(ContentProviderOperation.newUpdate(existingUri)
-                        .withValue(Locations.UUID, uuid)
-                        .withValue(Locations.PARENT_UUID, parentUuid)
-                        .build());
-                    syncResult.stats.numUpdates++;
+                if (!dbLocationNames.containsKey(locationUuid)) {
+                    dbLocationNames.put(locationUuid, new HashMap<String, String>());
                 }
 
-                if (location.names != null
-                    && (locationNames == null || !location.names.equals(locationNames))) {
-                    Uri existingNamesUri = namesUri.buildUpon().appendPath(
-                        String.valueOf(uuid)).build();
-                    // Update location names by deleting any existing location names and
-                    // repopulating.
-                    batch.add(ContentProviderOperation.newDelete(existingNamesUri).build());
-                    syncResult.stats.numDeletes++;
-                    for (String locale : location.names.keySet()) {
+                dbLocationNames.get(locationUuid).put(locale, name);
+            }
+        }
 
-                        batch.add(ContentProviderOperation.newInsert(existingNamesUri)
-                            .withValue(LocationNames.LOCATION_UUID, uuid)
-                            .withValue(LocationNames.LOCALE, locale)
-                            .withValue(LocationNames.NAME, location.names.get(locale))
+        try (Cursor c = contentResolver.query(uri, projection, null, null, null)) {
+            LOG.i("Examining locations: %d local, %d from server", c.getCount(), locations.size());
+            // Iterate through the list of locations
+            while (c.moveToNext()) {
+                syncResult.stats.numEntries++;
+
+                uuid = c.getString(c.getColumnIndex(Locations.UUID));
+                parentUuid = c.getString(c.getColumnIndex(Locations.PARENT_UUID));
+
+                JsonLocation location = locationsByUuid.get(uuid);
+                if (location != null) {
+                    // Entry exists. Remove from entry map to prevent insert later.
+                    locationsByUuid.remove(uuid);
+
+                    // Grab the names stored in the database for this location.
+                    Map<String, String> locationNames = dbLocationNames.get(uuid);
+
+                    // Check to see if the entry needs to be updated
+                    Uri existingUri = uri.buildUpon().appendPath(String.valueOf(uuid)).build();
+
+                    if (location.parent_uuid != null && !location.parent_uuid.equals(parentUuid)) {
+                        // Update existing record
+                        LOG.i("  - will update location " + uuid);
+                        batch.add(ContentProviderOperation.newUpdate(existingUri)
+                            .withValue(Locations.UUID, uuid)
+                            .withValue(Locations.PARENT_UUID, parentUuid)
                             .build());
-                        syncResult.stats.numInserts++;
+                        syncResult.stats.numUpdates++;
                     }
+
+                    if (location.names != null
+                        && (locationNames == null || !location.names.equals(locationNames))) {
+                        Uri existingNamesUri = namesUri.buildUpon().appendPath(
+                            String.valueOf(uuid)).build();
+                        // Update location names by deleting any existing location names and
+                        // repopulating.
+                        batch.add(ContentProviderOperation.newDelete(existingNamesUri).build());
+                        syncResult.stats.numDeletes++;
+                        for (String locale : location.names.keySet()) {
+
+                            batch.add(ContentProviderOperation.newInsert(existingNamesUri)
+                                .withValue(LocationNames.LOCATION_UUID, uuid)
+                                .withValue(LocationNames.LOCALE, locale)
+                                .withValue(LocationNames.NAME, location.names.get(locale))
+                                .build());
+                            syncResult.stats.numInserts++;
+                        }
+                    }
+                } else {
+                    // Entry doesn't exist. Remove it from the database.
+                    LOG.i("  - will delete location " + uuid);
+                    Uri deleteUri = uri.buildUpon().appendPath(uuid).build();
+                    batch.add(ContentProviderOperation.newDelete(deleteUri).build());
+                    syncResult.stats.numDeletes++;
+                    Uri namesDeleteUri = namesUri.buildUpon().appendPath(uuid).build();
+                    batch.add(ContentProviderOperation.newDelete(namesDeleteUri).build());
+                    syncResult.stats.numDeletes++;
                 }
-            } else {
-                // Entry doesn't exist. Remove it from the database.
-                LOG.i("  - will delete location " + uuid);
-                Uri deleteUri = uri.buildUpon().appendPath(uuid).build();
-                batch.add(ContentProviderOperation.newDelete(deleteUri).build());
-                syncResult.stats.numDeletes++;
-                Uri namesDeleteUri = namesUri.buildUpon().appendPath(uuid).build();
-                batch.add(ContentProviderOperation.newDelete(namesDeleteUri).build());
-                syncResult.stats.numDeletes++;
             }
         }
-        c.close();
 
         for (JsonLocation location : locationsByUuid.values()) {
             LOG.i("  - will insert location " + location.uuid);
