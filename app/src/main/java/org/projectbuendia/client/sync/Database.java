cc0e5a0d8aed171abf7a07afd3040a059aab8bd8
Fabian Tamp <fabian.tamp@gmail.com>
2015-11-13 10:34:16 +0800

    Relax "not null" constraint on local Observation UUIDs.
    Fixes #144.

--- app/src/main/java/org/projectbuendia/client/sync/Database.java
+++ app/src/main/java/org/projectbuendia/client/sync/Database.java
@@ -112,7 +112,11 @@ public class Database extends SQLiteOpenHelper {
             + "UNIQUE (location_uuid, locale)");
 
         SCHEMAS.put(Table.OBSERVATIONS, ""
-            + "uuid TEXT PRIMARY KEY NOT NULL,"
+            // uuid intentionally allows null values, because temporary observations inserted
+            // locally after submitting a form don't have UUIDs. Note that PRIMARY KEY in SQLite
+            // (and many other databases) treats all NULL values as different from all other values,
+            // so it's still ok to insert multiple records with a NULL UUID.
+            + "uuid TEXT PRIMARY KEY,"
             + "patient_uuid TEXT,"
             + "encounter_uuid TEXT,"
             + "encounter_millis INTEGER,"
