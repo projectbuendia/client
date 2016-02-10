59b52b25c006b8094b6cb46c73e22f4708c22476
Dan Cunningham <daniel.cunningham@gmail.com>
2016-02-10 13:33:30 +0100

    Fix css colours in formats

--- app/src/main/java/org/projectbuendia/client/ui/chart/Row.java
+++ app/src/main/java/org/projectbuendia/client/ui/chart/Row.java
@@ -14,13 +14,13 @@ public class Row {
     static Map<String, ChartItem> DEFAULTS = new HashMap<>();
     static {
         DEFAULTS.put("select_one", new ChartItem("", "", false, null, "{1,abbr}", "{1,name}", "", "", ""));
-        DEFAULTS.put("yes_no", new ChartItem("", "", false, null, "{1,yes_no,\u25cf;\u25cb}", "{1,yes_no,Yes;No}", "", "{1,yes_no,color:#000;color:#ccc}", ""));
-        DEFAULTS.put("yes_no_unknown", new ChartItem("", "", false, null, "{1,yes_no_unknown,\u25cf;\u25cb;?}", "{1,yes_no,Yes;No;Unknown}", "", "{1,yes_no,color:#000;color:#ccc}", ""));
+        DEFAULTS.put("yes_no", new ChartItem("", "", false, null, "{1,yes_no,\u25cf;\u25cb}", "{1,yes_no,Yes;No}", "", "{1,yes_no,color:black;color:DarkGray}", ""));
+        DEFAULTS.put("yes_no_unknown", new ChartItem("", "", false, null, "{1,yes_no_unknown,\u25cf;\u25cb;?}", "{1,yes_no,Yes;No;Unknown}", "", "{1,yes_no,color:black;color:DarkGray}", ""));
         DEFAULTS.put("number", new ChartItem("", "", false, null, "0", "0", "", "", ""));
         DEFAULTS.put("text", new ChartItem("", "", false, null, "{1,text,5}", "{1,text,40}", "", "", ""));
         DEFAULTS.put("date", new ChartItem("", "", false, null, "{1,date,MMM dd}", "{1,date,MMM dd}", "", "", ""));
         DEFAULTS.put("time", new ChartItem("", "", false, null, "{1,time,HH:mm}", "{1,time,HH:mm}", "", "", ""));
-        DEFAULTS.put("severity_bars", new ChartItem("", "", false, null, "{1,select,1107:\u25cb;1498:\u002d;1499:\u003d;1500:\u2261}", "{1,name}", "{1,select,1500:critical}", "{1,select,1107:color:#ccc}", ""));
+        DEFAULTS.put("severity_bars", new ChartItem("", "", false, null, "{1,select,1107:\u25cb;1498:\u002d;1499:\u003d;1500:\u2261}", "{1,name}", "{1,select,1500:critical}", "{1,select,1107:color:DarkGray}", ""));
     }
 
     public Row(@Nonnull ChartItem item) {
