67971fc8a1f3a80845185baf72c5ec7e287f1728
Kristen Tonga <krtonga@kristens-air.attlocal.net>
2019-06-19 19:36:27 -0700

    Add back button to FormEntryActivity

--- third_party/odkcollect/src/main/java/org/odk/collect/android/activities/FormEntryActivity.java
+++ third_party/odkcollect/src/main/java/org/odk/collect/android/activities/FormEntryActivity.java
@@ -242,6 +242,8 @@ public class FormEntryActivity
 		setContentView(R.layout.form_entry);
 
         setTitle(getString(R.string.title_loading_form));
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 //
 //		setTitle(getString(R.string.app_name) + " > "
 //				+ getString(R.string.loading_form));
@@ -1011,9 +1013,14 @@ public class FormEntryActivity
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		FormController formController = Collect.getInstance()
-				.getFormController();
+//		FormController formController = Collect.getInstance()
+//				.getFormController();
 		switch (item.getItemId()) {
+			case android.R.id.home :
+				// Go back rather than reloading the activity, so that the patient list retains its
+				// filter state.
+				onBackPressed();
+				return true;
 ////		case MENU_LANGUAGES:
 ////			Collect.getInstance()
 ////					.getActivityLogger()
