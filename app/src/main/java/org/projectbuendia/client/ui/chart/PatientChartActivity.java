a309d4228987b3a2e06e67abf58c5a4c0f7e9b42
Kristen Tonga <krtonga@kristens-air.lan>
2019-06-26 00:16:28 -0700

    Add back button to BaseLoggedInActivity

--- app/src/main/java/org/projectbuendia/client/ui/chart/PatientChartActivity.java
+++ app/src/main/java/org/projectbuendia/client/ui/chart/PatientChartActivity.java
@@ -175,17 +175,6 @@ public final class PatientChartActivity extends BaseLoggedInActivity {
         Utils.showIf(mPcr, ebolaLabTestFormEnabled);
     }
 
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            // Go back rather than reloading the activity, so that the patient list retains its
-            // filter state.
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override protected void onCreateImpl(Bundle savedInstanceState) {
         super.onCreateImpl(savedInstanceState);
         setContentView(R.layout.fragment_patient_chart);
@@ -255,9 +244,6 @@ public final class PatientChartActivity extends BaseLoggedInActivity {
             controllerState,
             mSyncManager,
             minimalHandler);
-
-        // Show the Up button in the action bar.
-        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         mAdmissionDaysView.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
